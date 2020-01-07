24a395ae695be03a0a63db9e0d8d5af9a026fc1b
Sindharta Tanuwijaya <sindharta@gmail.com>
2020-01-07 18:11:11 +0900

    Add a test to make sure that the sample scene is Valid

--- StreamingImageSequence~/Assets/Tests/Editor/SampleSceneTest.cs
+++ StreamingImageSequence~/Assets/Tests/Editor/SampleSceneTest.cs
@@ -1,7 +1,11 @@
 using NUnit.Framework;
+using System.IO;
 using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.Playables;
 using UnityEngine.StreamingImageSequence;
-
+using UnityEngine.Timeline;
 
 namespace Tests {
     public class SampleSceneTest {
@@ -19,5 +23,27 @@ namespace Tests {
             }
         }
 
+//---------------------------------------------------------------------------------------------------------------------
+        [Test]
+        public void SampleSceneValidityPass() {           
+            string sampleScenePath = "Assets/Scenes/defaultSample.unity";
+            Assert.IsTrue(File.Exists(sampleScenePath));
+            EditorSceneManager.OpenScene(sampleScenePath);
+
+            PlayableDirector[] pds = Object.FindObjectsOfType<PlayableDirector>(); 
+            foreach (PlayableDirector pd in pds) {
+                PlayableAsset playableAsset = pd.playableAsset;
+
+                if (!(playableAsset is TimelineAsset)) {
+                    continue;
+                }
+
+                TimelineAsset timelineAsset = playableAsset as TimelineAsset;
+                foreach (TrackAsset trackAsset in timelineAsset.GetOutputTracks()) {
+                    //Make sure the Image is bound to the trackAsset
+                    Assert.IsNotNull(pd.GetGenericBinding(trackAsset));
+                }
+            }
+        }
     }
 }
