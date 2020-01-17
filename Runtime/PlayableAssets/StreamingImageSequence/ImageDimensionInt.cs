2ce5019d1339e43261b6a020cef1766f0569bffc
Sindharta Tanuwijaya <sindharta@gmail.com>
2020-01-17 17:55:21 +0900

    Add a function to calculate width/height ratio in ImageDimensionInt

--- Runtime/PlayableAssets/StreamingImageSequence/ImageDimensionInt.cs
+++ Runtime/PlayableAssets/StreamingImageSequence/ImageDimensionInt.cs
@@ -1,10 +1,14 @@
 ﻿namespace UnityEngine.StreamingImageSequence {
 
     [System.Serializable]
-    public struct ImageDimensionInt
-    {
+    public struct ImageDimensionInt {
         public int Width;
         public int Height;
+
+        public float CalculateRatio() {
+            return Width / (float) Height;
+        }
+
     }
 
 } //end namespace
