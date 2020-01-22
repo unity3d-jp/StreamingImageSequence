8e0cb5bc12eca4f7dd228b47a4de669542d2bcf0
Sindharta Tanuwijaya <sindharta@gmail.com>
2020-01-22 15:34:38 +0900

    Add a test to load image

---
+++ Tests/Runtime/ImageLoadTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using System.IO;
+using UnityEngine;
+using UnityEngine.StreamingImageSequence;
+using UnityEngine.TestTools;
+
+namespace UnityEditor.StreamingImageSequence.Tests {
+    public class ImageLoadTest {
+        [UnityTest]
+        public IEnumerator QueueImageLoadTask() {
+            string fullPath = "Packages/com.unity.streaming-image-sequence/Tests/Data/png/A_00000.png";
+            Assert.IsTrue(File.Exists(fullPath));
+
+            StreamingImageSequencePlugin.GetNativTextureInfo(fullPath, out ReadResult readResult);
+            Assert.AreEqual(readResult.ReadStatus, 0);
+
+            new BGJobPictureLoader(fullPath);
+            yield return new WaitForSeconds(1.0f);
+
+            StreamingImageSequencePlugin.ResetNativeTexture(fullPath);
+            Assert.AreEqual(readResult.ReadStatus, 0);
+        }
+
+
+    }
+
+} //end namespace
