6a8f5ae654c48499ac3996f2abe6253899e539f5
Sindharta Tanuwijaya <sindharta@gmail.com>
2020-02-04 19:38:50 +0900

    Moving LoaderWin-related files to Win folder

---
+++ Plugins~/Loader/Win/LoaderWin.cpp
@@ -0,0 +1,78 @@
+// LoaderWin.cpp : Win implementation of Loader APIs
+//
+
+#include "stdafx.h"
+
+#include "CommonLib/Types.h"
+#include "CommonLib/ReadResult.h"
+
+#pragma comment( lib, "winmm.lib" )
+#pragma comment(lib, "gdiplus.lib")
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+void* loadPNGFileAndAlloc(const charType* fileName, StReadResult* pResult)
+{
+	u8* pBuffer = NULL;
+
+# if USE_WCHAR
+	Gdiplus::Bitmap*    pBitmap = Gdiplus::Bitmap::FromFile(fileName);
+# else
+	const int size = 8192;
+	WCHAR wlocal[size] = { 0x00 };
+
+
+	int ret = MultiByteToWideChar(
+		CP_ACP,
+		MB_PRECOMPOSED,
+		fileName,
+		strlen(fileName),
+		wlocal,
+		size);
+	Gdiplus::Bitmap*    pBitmap = Gdiplus::Bitmap::FromFile(wlocal);
+
+# endif
+	Gdiplus::Status status = Gdiplus::FileNotFound;
+	if (pBitmap )
+	{
+		status = pBitmap->GetLastStatus();
+	}
+	ASSERT(status == Gdiplus::Ok);
+	if (status == Gdiplus::Ok)
+	{
+
+		u32 width = pBitmap->GetWidth();
+		u32 height = pBitmap->GetHeight();
+
+		pBuffer = (u8*)malloc(sizeof(u32)* width * height);
+		ASSERT(pBuffer !=nullptr);
+		u32* pImage = (u32*)pBuffer;
+
+		pResult->width = width;
+		pResult->height = height;
+		pResult->buffer = pBuffer;
+
+		Gdiplus::BitmapData bitmapData;
+		pBitmap->LockBits(&Gdiplus::Rect(0, 0, width, height), Gdiplus::ImageLockModeWrite, PixelFormat32bppARGB, &bitmapData);
+		u32 *pRawBitmapOrig = (u32*)bitmapData.Scan0;   // for easy access and indexing
+		for (u32 yy = 0; yy < height; yy++)
+		{
+			memcpy(&pImage[yy*width], &pRawBitmapOrig[(height - 1 - yy) * bitmapData.Stride / 4], width*sizeof(u32));
+
+		}
+
+		//memcpy(pImage, bitmapData.Scan0, width*height*sizeof(UINT32));
+		pBitmap->UnlockBits(&bitmapData);
+		delete pBitmap;
+	}
+	else
+	{
+		ASSERT(0);
+	}
+
+
+	ASSERT(pBuffer);
+	return pBuffer; //  pBuffer;
+}
+
