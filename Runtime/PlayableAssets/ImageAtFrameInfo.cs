3dffc1c9ef4f8e08bed9d7dcf3b0874cc8d8f331
Sindharta Tanuwijaya <>
2020-02-07 21:29:15 +0900

    Add ImageAtFrameInfo to store if we should use the image at a particular frame

---
+++ Runtime/PlayableAssets/ImageAtFrameInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UnityEngine.StreamingImageSequence {
+    
+[Serializable]
+public class ImageAtFrameInfo {
+
+    internal ImageAtFrameInfo(UseImageMarker marker, double localTime) {
+        m_useImage = true;
+        m_localTime = localTime;
+    }
+    
+//----------------------------------------------------------------------------------------------------------------------
+    internal bool IsUsed() { return m_useImage; }
+    internal void SetUsed(bool used) { m_useImage = used; }
+
+//----------------------------------------------------------------------------------------------------------------------
+
+    [SerializeField] private bool m_useImage;
+    [SerializeField] private double m_localTime;
+
+}
+
+
+} //end namespace
+
+
+//A structure to store if we should use the image at a particular frame
