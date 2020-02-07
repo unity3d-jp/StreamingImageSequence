e34d379a0c02ace1f6080d8bb6b47a0f6d5c91af
Sindharta Tanuwijaya <>
2020-02-07 18:11:21 +0900

    Add UseImageMarker to indicate if we want to use an image in a particular frame

---
+++ Runtime/PlayableAssets/UseImageMarker.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+namespace UnityEngine.StreamingImageSequence {
+
+internal class UseImageMarker : Marker, INotification {
+
+    ~UseImageMarker() {
+        m_playableAsset.HideImage(time);
+    }
+
+//----------------------------------------------------------------------------------------------------------------------    
+    internal void SetPlayableAsset(StreamingImageSequencePlayableAsset playableAsset) {
+        m_playableAsset = playableAsset;
+    } 
+    
+    internal StreamingImageSequencePlayableAsset GetPlayableAsset() {  return m_playableAsset; }
+    
+//----------------------------------------------------------------------------------------------------------------------    
+    public PropertyName id { get; } //use default implementation
+
+    [SerializeField] private StreamingImageSequencePlayableAsset m_playableAsset = null;
+}
+
+} //end namespace
