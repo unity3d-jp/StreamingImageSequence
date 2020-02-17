63c3d2b6df24fe6e89db146e6ed5797051d0abda
Sindharta Tanuwijaya <sindharta@gmail.com>
2020-02-17 15:18:07 +0900

    Use SerializedProperty in UseImageMarker to enable multiple object editing

--- Editor/UseImageMarkerInspector.cs
+++ Editor/UseImageMarkerInspector.cs
@@ -10,18 +10,24 @@ public class UseImageMarkerInspector: Editor {
 
     void OnEnable() {
         m_asset = target as UseImageMarker;
+        m_useImageProperty = serializedObject.FindProperty("m_info.m_useImage");
+        m_useImageContent = new GUIContent("Use Image");
     }
 
 //----------------------------------------------------------------------------------------------------------------------
     public override void OnInspectorGUI() {
         //base.OnInspectorGUI();
-        m_asset.SetImageUsed(EditorGUILayout.Toggle("Show Image", m_asset.IsImageUsed()));
+        serializedObject.Update ();
+
+        EditorGUILayout.PropertyField(m_useImageProperty, m_useImageContent);
+        serializedObject.ApplyModifiedProperties ();
     }
 
 //----------------------------------------------------------------------------------------------------------------------
 
-    UseImageMarker m_asset = null;
-
+    private UseImageMarker m_asset = null;
+    private GUIContent m_useImageContent = null;
+    private SerializedProperty m_useImageProperty; //For multiple object editing
 }
 
 } //end namespace
