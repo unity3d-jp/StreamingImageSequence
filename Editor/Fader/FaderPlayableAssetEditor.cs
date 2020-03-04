8d76d1d64d63e715890d9d16bd7ede34ba00790a
Sindharta Tanuwijaya <sindharta@gmail.com>
2020-03-04 13:11:09 +0900

    Highlight FaderColor

---
+++ Editor/Fader/FaderPlayableAssetEditor.cs
@@ -0,0 +1,21 @@
+﻿using JetBrains.Annotations;
+using UnityEditor.Timeline;
+using UnityEngine.Timeline;
+
+namespace UnityEngine.StreamingImageSequence {
+
+    [CustomTimelineEditor(typeof(FaderPlayableAsset)), UsedImplicitly]
+    internal class FaderPlayableAssetEditor : ClipEditor {
+        /// <inheritdoc/>
+        public override ClipDrawOptions GetClipOptions(TimelineClip clip) {
+            ClipDrawOptions clipOptions = base.GetClipOptions(clip);
+            FaderPlayableAsset asset = clip.asset as FaderPlayableAsset;
+            if (null == asset)
+                return clipOptions;
+
+            clipOptions.highlightColor = asset.GetColor();            
+            return clipOptions;
+        }
+
+    }
+}
