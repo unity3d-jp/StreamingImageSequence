8a2fd209c47db8514c97f600b1666432c2d0be33
Sindharta Tanuwijaya <>
2020-03-30 17:10:57 +0900

    Add a shortcut to enable/disable image marker

---
+++ Editor/Shortcuts.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEditor.ShortcutManagement;
+
+namespace UnityEngine.StreamingImageSequence {
+
+public static class Shortcuts  {
+
+    [Shortcut("StreamingImageSequence/Toggle Use Image Marker", null,  KeyCode.U)]
+    static void ToggleUseImageMarker(ShortcutArguments args) {
+        foreach (Object obj in Selection.objects) {
+            UseImageMarker marker = obj as UseImageMarker;
+            if (null == marker) {
+                continue;
+            }
+            
+            marker.SetImageUsed(!marker.IsImageUsed());
+
+        }
+        
+    }    
+    
+}
+
+} //end namespace
