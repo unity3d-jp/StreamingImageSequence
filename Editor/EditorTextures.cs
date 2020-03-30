0901212c64a66e0ae7f15b035bf9cfbe1bbb31a9
Sindharta Tanuwijaya <>
2020-03-30 18:22:49 +0900

    Adding EditorTextures to load checked texture in the editor

---
+++ Editor/EditorTextures.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+
+namespace UnityEngine.StreamingImageSequence {
+
+internal static class EditorTextures {
+
+    internal static Texture GetCheckedTexture() { return m_checkedTexture;}
+    
+//----------------------------------------------------------------------------------------------------------------------
+
+    [InitializeOnLoadMethod]
+    static void OnLoad() {
+        const string FULL_PATH = "Packages/com.unity.streaming-image-sequence/Editor/Textures/Checked.png";
+        m_checkedTexture = AssetDatabase.LoadAssetAtPath<Texture>(FULL_PATH);
+    }
+    
+//----------------------------------------------------------------------------------------------------------------------
+
+    private static Texture m_checkedTexture;
+
+}
+
+} //end namespace
