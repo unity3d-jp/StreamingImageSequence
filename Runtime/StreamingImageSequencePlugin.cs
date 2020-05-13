b221345e9fd23b0c31173e6c90b5b89759806a18
Sindharta Tanuwijaya <sindharta@gmail.com>
2020-05-13 15:34:58 +0900

    Remove API functions which have been removed in the DLL

--- Runtime/StreamingImageSequencePlugin.cs
+++ Runtime/StreamingImageSequencePlugin.cs
@@ -32,11 +32,9 @@ namespace UnityEngine.StreamingImageSequence {
 
 #if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
         private const string LOADER_DLL             = "Loader";
-        private const string DRAWER_DLL             = "Drawer";
         private const string DRAW_OVER_WINDOW_DLL   = "DrawOverWindow";
 #elif UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
         private const string LOADER_DLL             = "Project";
-        private const string DRAWER_DLL             = "Project";
         private const string DRAW_OVER_WINDOW_DLL   = "Project";
 #endif
 
@@ -67,21 +65,10 @@ namespace UnityEngine.StreamingImageSequence {
         [DllImport(LOADER_DLL, CharSet = CharSet.Unicode, ExactSpelling = true)]
         public static extern int   IsPluginResetting();
 
-        // Implemented in Drawer dll
-        [DllImport(DRAWER_DLL, CharSet = CharSet.Unicode, ExactSpelling = true)]
-        public static extern void SetNativeTexturePtr(IntPtr Texture, UInt32 uWidth, UInt32 height, Int32 sObjectID);
 
-        [DllImport(DRAWER_DLL, CharSet = CharSet.Unicode, ExactSpelling = true)]
-        public static extern void SetLoadedTexture([MarshalAs(UnmanagedType.LPStr)]string fileName, Int32 sObjectID);
-
-        [DllImport(DRAWER_DLL, CharSet = CharSet.Unicode, ExactSpelling = true)]
-        public static extern void ResetLoadedTexture(Int32 sObjectID);
-
-        [DllImport(DRAWER_DLL, CharSet = CharSet.Unicode, ExactSpelling = true)]
+        [DllImport(LOADER_DLL, CharSet = CharSet.Unicode, ExactSpelling = true)]
         public static extern void ResetAllLoadedTexture();
 
-        [DllImport(DRAWER_DLL, CharSet = CharSet.Unicode, ExactSpelling = true)]
-        public static extern IntPtr GetRenderEventFunc();
 
 #if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
         [DllImport(DRAW_OVER_WINDOW_DLL, CharSet = CharSet.Unicode, ExactSpelling = true)]
@@ -124,11 +111,6 @@ namespace UnityEngine.StreamingImageSequence {
 
 #endif //UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN || UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
 
-//----------------------------------------------------------------------------------------------------------------------
-
-        public static bool IsResetting() {
-            return (StreamingImageSequencePlugin.IsPluginResetting() != 0);
-        }
 
 //----------------------------------------------------------------------------------------------------------------------
 
@@ -140,19 +122,21 @@ namespace UnityEngine.StreamingImageSequence {
                 StreamingImageSequenceConstants.TEXTURE_FORMAT, false, false
             );
 
+            tex.filterMode = FilterMode.Bilinear;
+            return tex;
+        }
+
+        public static void CopyBufferToTexture(this ReadResult readResult, Texture2D tex) {
+            int length = readResult.Width * readResult.Height * 4;
             unsafe {
                 void* src = readResult.Buffer.ToPointer();
                 NativeArray<float> rawTextureData = tex.GetRawTextureData<float>();
                 void* dest = rawTextureData.GetUnsafePtr();
                 Buffer.MemoryCopy(src, dest, length, length);
             }
-            tex.filterMode = FilterMode.Bilinear;
             tex.Apply();
-
-
-            return tex;
-
-        }
+            
+        }        
     }
 
 }
