703d3f5eaf0b4b0247d0819c788f6cd89fa1b757
Sindharta Tanuwijaya <>
2020-05-28 22:20:25 +0900

    Set timelineclip of RenderCachePlayableAsset during on Create

---
+++ Editor/RenderCache/RenderCachePlayableAssetEditor.cs
@@ -0,0 +1,47 @@
+﻿using JetBrains.Annotations;
+using UnityEditor.Timeline;
+using UnityEngine.Timeline;
+using UnityEngine;
+using UnityEngine.StreamingImageSequence;
+
+namespace UnityEditor.StreamingImageSequence {
+[CustomTimelineEditor(typeof(RenderCachePlayableAsset)), UsedImplicitly]
+internal class RenderCachePlayableAssetEditor : ClipEditor {
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+    /// <inheritdoc/>
+    public override void OnCreate(TimelineClip clip, TrackAsset track, TimelineClip clonedFrom)
+    {
+        RenderCachePlayableAsset asset = clip.asset as RenderCachePlayableAsset;
+        if (null == asset) {
+            Debug.LogError("Asset is not a RenderCachePlayableAsset: " + clip.asset);
+            return;
+        }
+
+        clip.parentTrack = track; 
+        asset.SetTimelineClip(clip);
+    }
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+    /// <inheritdoc/>
+    public override void DrawBackground(TimelineClip clip, ClipBackgroundRegion region) {
+        base.DrawBackground(clip, region);
+        
+        //[TODO-sin: 2020-5-27]
+        //Show preview icons
+        
+    }
+
+//----------------------------------------------------------------------------------------------------------------------
+    // readonly Dictionary<TimelineClip, StreamingImageSequencePreview> m_persistentPreviews 
+    //     = new Dictionary<TimelineClip, StreamingImageSequencePreview>();
+    //
+    // ColorSpace m_colorSpace = ColorSpace.Uninitialized;
+
+}
+
+}
