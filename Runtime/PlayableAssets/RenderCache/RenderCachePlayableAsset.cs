60c4e443cb8032b347cf25ff305534c9b5a9d3c8
Sindharta Tanuwijaya <>
2020-05-28 19:12:41 +0900

    Add a couple of properties in RenderCachePlayableAsset

--- Runtime/PlayableAssets/RenderCache/RenderCachePlayableAsset.cs
+++ Runtime/PlayableAssets/RenderCache/RenderCachePlayableAsset.cs
@@ -24,6 +24,7 @@ internal class RenderCachePlayableAsset : PlayableAsset {
         //Access the camera, and store the rendered results somewhere
         
     }
+
     
 //----------------------------------------------------------------------------------------------------------------------
     //TODO-sin: 2020-5-27: Factor out common code with StreamingImageSequenceTrack     
@@ -33,6 +34,14 @@ internal class RenderCachePlayableAsset : PlayableAsset {
     
     internal TimelineClip GetTimelineClip() { return m_timelineClip; }
     internal void SetTimelineClip(TimelineClip clip) { m_timelineClip = clip; }
+
+    
+//----------------------------------------------------------------------------------------------------------------------
+    internal string GetFolder() { return m_folder;}
+    internal void SetFolder(string folder) { m_folder = folder;}
+
+    internal bool GetDeleteImagesBeforeUpdating() { return m_deleteImagesBeforeUpdating;}
+    internal void SetDeleteImagesBeforeUpdating(bool deleteImages) { m_deleteImagesBeforeUpdating = deleteImages;}   
     
     
 //----------------------------------------------------------------------------------------------------------------------
@@ -49,6 +58,8 @@ internal class RenderCachePlayableAsset : PlayableAsset {
     
 //----------------------------------------------------------------------------------------------------------------------    
     private TimelineClip m_timelineClip = null;
+    [SerializeField] private string m_folder = null;
+    [SerializeField] private bool m_deleteImagesBeforeUpdating = true;
 
 
 }
