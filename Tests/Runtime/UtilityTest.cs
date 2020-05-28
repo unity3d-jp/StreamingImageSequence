a13066a750c74c0d4c5d143f99e59b6a2a197327
Sindharta Tanuwijaya <>
2020-05-28 19:45:37 +0900

    Add MathUtility

---
+++ Tests/Runtime/UtilityTest.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+
+namespace UnityEngine.StreamingImageSequence.Tests {
+
+public class UtilityTest {
+
+
+//----------------------------------------------------------------------------------------------------------------------    
+    
+    [Test]
+    public void VerifyNumDigits() {
+        Assert.AreEqual(1,MathUtility.GetNumDigits(0));
+        Assert.AreEqual(1,MathUtility.GetNumDigits(1));
+        Assert.AreEqual(1,MathUtility.GetNumDigits(9));
+
+        Assert.AreEqual(2,MathUtility.GetNumDigits(10));
+        Assert.AreEqual(2,MathUtility.GetNumDigits(11));
+        Assert.AreEqual(2,MathUtility.GetNumDigits(99));
+        
+        Assert.AreEqual(3,MathUtility.GetNumDigits(100));
+        Assert.AreEqual(3,MathUtility.GetNumDigits(101));
+        Assert.AreEqual(3,MathUtility.GetNumDigits(199));
+        Assert.AreEqual(3,MathUtility.GetNumDigits(999));
+        
+        //Negative
+        Assert.AreEqual(1,MathUtility.GetNumDigits(-1));
+        Assert.AreEqual(1,MathUtility.GetNumDigits(-9));
+
+        Assert.AreEqual(2,MathUtility.GetNumDigits(-10));
+        Assert.AreEqual(2,MathUtility.GetNumDigits(-11));
+        Assert.AreEqual(2,MathUtility.GetNumDigits(-99));
+        
+        Assert.AreEqual(3,MathUtility.GetNumDigits(-100));
+        Assert.AreEqual(3,MathUtility.GetNumDigits(-101));
+        Assert.AreEqual(3,MathUtility.GetNumDigits(-199));
+        Assert.AreEqual(3,MathUtility.GetNumDigits(-999));
+        
+    }
+    
+
+}
+
+} //end namespace
