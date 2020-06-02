ff94c5c3a49d6c66588695591fec5c9aba5d4088
Sindharta Tanuwijaya <sindharta.tanuwijaya@unity3d.com>
2020-06-02 23:16:19 +0900

    Fix the name of the module on Mac

--- Runtime/StreamingImageSequencePlugin.cs
+++ Runtime/StreamingImageSequencePlugin.cs
@@ -18,8 +18,7 @@ namespace UnityEngine.StreamingImageSequence {
         private const string LOADER_DLL             = "Loader";
         private const string DRAW_OVER_WINDOW_DLL   = "DrawOverWindow";
 #elif UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
-        private const string LOADER_DLL             = "Project";
-        private const string DRAW_OVER_WINDOW_DLL   = "Project";
+        private const string LOADER_DLL             = "Loader";
 #endif
 
         // Implemented in Loader dll
