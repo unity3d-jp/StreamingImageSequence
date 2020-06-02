279191da83f48acf71e5f3dd171479200479a66a
Sindharta Tanuwijaya <sindharta@users.noreply.github.com>
2020-06-02 14:05:23 +0900

    feat: initial implementation of RenderCache (#75)
    * Add a scene for testing RenderCache
    * Remove IDIsposable from StreamingImageSequencePreview. Unused
    * Depend on editorcoroutines
    * Use PlayableAsset's time to control the caching instead of director
    * Add clipCaps.None for RenderCachePlayableAsset
    * Display folder selector in the GUI of RenderCacheCreator
    * Set the Timelineclip of RenderCachePlayableAsset during onCreate
    * Show the progress of rendering the cache in game view
    * Can cancel the process to cache render results
    * Check if the assigned camera is active before rendering cache
    * Show capture resolution
    
    Others
    * Add post processing in the test project
    
    Utilities:
    * Find Timeline asset's director using FindObjects
    * Add MathUtility to calculate the number of digits
    * Add a utility function to delete asset

---
+++ Editor/RenderCache/RenderCachePreview.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.StreamingImageSequence;
+
+namespace UnityEditor.StreamingImageSequence {
+
+internal class RenderCachePreview {
+
+    public RenderCachePreview(RenderCachePlayableAsset playableAsset) {
+        m_playableAsset = playableAsset;
+    }
+
+
+
+//----------------------------------------------------------------------------------------------------------------------
+    private readonly RenderCachePlayableAsset m_playableAsset = null;
+}
+
+} //end namespace
+
