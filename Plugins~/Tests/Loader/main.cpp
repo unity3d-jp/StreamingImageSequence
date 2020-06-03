ba0b5cba0ca8f6983cae9dfebf5978f3d0be809d
sindharta <sindharta@gmail.com>
2020-06-03 18:12:04 +0900

    use lib when we want to run unit tests.

--- Plugins~/Tests/Loader/main.cpp
+++ Plugins~/Tests/Loader/main.cpp
@@ -1,5 +1,19 @@
 
-void main() {
+#include <gtest/gtest.h>
+#include "Loader/Loader.h"
+
+TEST(TestSuiteName, TestName) {
+    ASSERT_EQ(0,1) << "Vectors x and y are of unequal length";
+}
+
+
+int main(int argc, char** argv)
+{
+
+    ResetPlugin();
+
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
 
 
