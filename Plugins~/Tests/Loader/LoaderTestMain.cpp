17dcdd343c66feb3140fb82f75c303ab9067dff2
sindharta <sindharta@gmail.com>
2020-06-10 15:53:40 +0900

    Add test for testing automatic memory deallocation

--- Plugins~/Tests/Loader/LoaderTestMain.cpp
+++ Plugins~/Tests/Loader/LoaderTestMain.cpp
@@ -61,6 +61,21 @@ void CheckMemoryCleanup() {
     ASSERT(0 == GetNumLoadedTextures(CRITICAL_SECTION_TYPE_PREVIEW_IMAGE));
 }
 
+//----------------------------------------------------------------------------------------------------------------------
+
+uint32_t FindNumDuplicateMapElements(
+    const std::map<strType, StreamingImageSequencePlugin::ImageData>& map0, 
+    const std::map<strType, StreamingImageSequencePlugin::ImageData>& map1) 
+{
+    uint32_t ret = 0;
+    for (auto itr = map0.begin(); itr != map0.end(); ++itr) {
+        if (map1.find(itr->first)!=map1.end()) {
+            ++ret;
+        }
+
+    }
+    return ret;
+}
 
 //----------------------------------------------------------------------------------------------------------------------
 
@@ -117,6 +132,63 @@ TEST(Loader, LoadMultipleImagesTest) {
     //Unload
     imageCatalog.UnloadAllImages();
     CheckMemoryCleanup();
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+TEST(Loader, AutoUnloadUnusedImagesTest) {
+    using namespace StreamingImageSequencePlugin;
+    ImageCatalog& imageCatalog = ImageCatalog::GetInstance();
+
+    int curFrame = 0;
+    bool processed = LoadTestImages(CRITICAL_SECTION_TYPE_FULL_IMAGE, curFrame, 0, 1);
+    ASSERT_EQ(true, processed);
+
+    const uint64_t reqMemForOneImage = imageCatalog.GetUsedMemory();
+    ASSERT_GT(reqMemForOneImage, 0);
+    const uint32_t numImagesLimit = static_cast<uint32_t>(std::floor( static_cast<float>(MAX_IMAGE_MEMORY) / reqMemForOneImage));
+    ASSERT(numImagesLimit < NUM_TEST_IMAGES);
+
+    //Load the remaining images
+    processed = LoadTestImages(CRITICAL_SECTION_TYPE_FULL_IMAGE, curFrame, 1, numImagesLimit-1);
+    bool readSuccessful = CheckLoadedTestImageData(CRITICAL_SECTION_TYPE_FULL_IMAGE, curFrame, 0, numImagesLimit);
+    ASSERT_EQ(true, processed);
+    ASSERT_EQ(true, readSuccessful) << "Loading image failed";
+    std::map<strType, ImageData> images_0 = imageCatalog.GetImageMap(CRITICAL_SECTION_TYPE_FULL_IMAGE);
+
+    //Load images for frame 1
+    ++curFrame;
+    uint32_t startIndex = numImagesLimit;
+    const uint32_t numImages_1 = 3;
+    ASSERT(startIndex + numImages_1 -1 < NUM_TEST_IMAGES);
+    processed = LoadTestImages(CRITICAL_SECTION_TYPE_FULL_IMAGE, curFrame, startIndex, numImages_1);
+    readSuccessful = CheckLoadedTestImageData(CRITICAL_SECTION_TYPE_FULL_IMAGE, curFrame, startIndex, numImages_1);
+    ASSERT_EQ(true, processed);
+    ASSERT_EQ(true, readSuccessful) << "Loading image failed";
+    std::map<strType, ImageData> images_1 = imageCatalog.GetImageMap(CRITICAL_SECTION_TYPE_FULL_IMAGE);
+    uint32_t numDuplicates = FindNumDuplicateMapElements(images_0, images_1);
+    ASSERT_EQ(images_0.size() - numImages_1, numDuplicates) << "Error in unloading unused images";
+
+    //Load images for frame 2
+    ++curFrame;
+    startIndex += numImages_1;
+    const uint32_t numImages_2 = 5;
+    ASSERT(startIndex + numImages_2 -1 < NUM_TEST_IMAGES);
+    processed = LoadTestImages(CRITICAL_SECTION_TYPE_FULL_IMAGE, curFrame, startIndex, numImages_2);
+    readSuccessful = CheckLoadedTestImageData(CRITICAL_SECTION_TYPE_FULL_IMAGE, curFrame, startIndex, numImages_2);
+    ASSERT_EQ(true, processed);
+    ASSERT_EQ(true, readSuccessful) << "Loading image failed";
+    std::map<strType, ImageData> images_2 = imageCatalog.GetImageMap(CRITICAL_SECTION_TYPE_FULL_IMAGE);
+    numDuplicates = FindNumDuplicateMapElements(images_1, images_2);
+    numDuplicates = FindNumDuplicateMapElements(images_1, images_2);
+    ASSERT_EQ(images_1.size() - numImages_2, numDuplicates) << "Error in unloading unused images";
+
+
+    //Unload
+    imageCatalog.UnloadAllImages();
+    CheckMemoryCleanup();
+
+
 }
 
 //----------------------------------------------------------------------------------------------------------------------
