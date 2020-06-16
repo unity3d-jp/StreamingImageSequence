57381894fc68321d90f7f03cb31ef89ef5689f33
Sindharta Tanuwijaya <sindharta@users.noreply.github.com>
2020-06-16 13:20:41 +0900

    feat: integration of Pencil+ Line with RenderCache (#89)
    * unify the version defines naming scheme
    * add a reference to com.unity.pencil-line asmdef
    * add version defines: AT_USE_PENCILLINE
    * add PencilLineRTCapturer: a component to capture the render texture of Pencil+ Line
    * add a new scene to test the capturing of Pencil+ Line
    
    gitignore:
    * Ignore internally developed package

---
+++ Runtime/Scripts/RenderCache/Components/PencilLineRTCapturer.cs
@@ -0,0 +1,58 @@
+﻿#if AT_USE_PENCILLINE
+
+using Pencil_4;
+
+namespace UnityEngine.StreamingImageSequence {
+
+
+[ExecuteAlways]
+internal class PencilLineRTCapturer : BaseRTCapturer {
+
+
+    /// <inheritdoc/>
+    public override bool BeginCapture() {
+        if (null == m_pencilLineEffect) {
+            SetErrorMessage("PencilLineEffect is not set on " + gameObject.name);
+            return false;
+        }
+
+        PencilLineRenderer lineRenderer = m_pencilLineEffect.PencilRenderer;
+        if (null == lineRenderer) {
+            SetErrorMessage("PencilLineEffect doesn't have PencilRenderer on " + gameObject.name);
+            return false;
+        }
+
+        ReleaseRenderTexture();
+        m_pencilTex = lineRenderer.Texture;
+
+        m_rt = new RenderTexture(m_pencilTex.width, m_pencilTex.height, 0);
+        m_rt.Create();
+        
+        return true;
+    }
+
+    /// <inheritdoc/>
+    public override void EndCapture() {        
+        ReleaseRenderTexture();
+    }
+    
+    
+//----------------------------------------------------------------------------------------------------------------------
+
+    /// <inheritdoc/>
+    protected override RenderTexture UpdateRenderTexture() {
+        Graphics.Blit(m_pencilTex, m_rt);
+        return m_rt;
+    }
+    
+    
+//----------------------------------------------------------------------------------------------------------------------
+    [SerializeField] private PencilLineEffect m_pencilLineEffect = null;
+
+    private Texture2D m_pencilTex = null;
+
+}
+
+} //end namespace
+
+#endif //AT_USE_PENCILLINE
