4a44ebc6d9ffedd1bdf12c972b9e16f6efb030e1
sindharta <sindharta@gmail.com>
2020-07-02 17:48:38 +0900

    Separate PeriodicJob to a different file

---
+++ Runtime/PeriodicJob.cs
@@ -0,0 +1,30 @@
+﻿namespace UnityEngine.StreamingImageSequence  { 
+
+    internal abstract class PeriodicJob  {
+        public UpdateManager.JobOrder m_order;
+        internal bool m_RemoveRequestFlag;
+        internal bool m_InitializedFlag;
+        public abstract void Execute();
+        public abstract void Initialize();
+        public abstract void Cleanup(); // Uninitialize
+        public abstract void Reset();   // called while resetting.
+
+        public  PeriodicJob(UpdateManager.JobOrder order) {
+            m_order = order;
+        }
+        public void AddToUpdateManger() {
+            UpdateManager.AddPeriodicJob( this);
+        }
+
+        public void RemoveFromUpdateManager() {
+            UpdateManager.RemovePeriodicJob(this);
+        }
+
+        public void RemoveIfFinished()
+        {
+            m_RemoveRequestFlag = true;
+        }
+    }
+
+} //end namespace
+
--- Runtime/UpdateManager.cs
+++ Runtime/UpdateManager.cs
@@ -506,38 +506,4 @@ namespace UnityEngine.StreamingImageSequence
         public abstract void Execute();
     }
 
-    internal abstract class PeriodicJob
-    {
-        public UpdateManager.JobOrder m_order;
-        internal bool m_RemoveRequestFlag;
-        internal bool m_InitializedFlag;
-        public abstract void Execute();
-        public abstract void Initialize();
-        public abstract void Cleanup(); // Uninitialize
-        public abstract void Reset();   // called while resetting.
-
-        private PeriodicJob()
-        {
-
-        }
-        public  PeriodicJob(UpdateManager.JobOrder order)
-        {
-            m_order = order;
-        }
-        public void AddToUpdateManger()
-        {
-            UpdateManager.AddPeriodicJob( this);
-        }
-
-        public void RemoveFromUpdateManager()
-        {
-            UpdateManager.RemovePeriodicJob(this);
-        }
-
-        public void RemoveIfFinished()
-        {
-            m_RemoveRequestFlag = true;
-        }
-    }
-
 }
