f09fec0d8e6dc9cf10c7103142d9c10bc0b559cb
sindharta <sindharta@gmail.com>
2020-07-08 20:06:44 +0900

    Add ImageLoadInfo

---
+++ Runtime/Scripts/Loader/ImageLoadInfo.cs
@@ -0,0 +1,26 @@
+﻿namespace UnityEngine.StreamingImageSequence {
+
+
+internal class ImageLoadInfo {
+
+
+//----------------------------------------------------------------------------------------------------------------------
+    internal  ImageLoadInfo( string imagePath, int requestFrame) {
+        m_imagePath = imagePath;
+        m_requestFrame = requestFrame;
+    }
+
+//----------------------------------------------------------------------------------------------------------------------
+    string GetImagePath()                     { return m_imagePath; }
+    
+    void SetRequestFrame(int requestFrame)    { m_requestFrame = requestFrame; } 
+    int GetRequestFrame()                     { return m_requestFrame; } 
+
+    
+//----------------------------------------------------------------------------------------------------------------------
+    private readonly string m_imagePath;
+    private int m_requestFrame;
+
+}
+
+} //end namespace
