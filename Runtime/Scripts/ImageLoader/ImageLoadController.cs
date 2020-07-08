843a680e1e4110e54597121eac39153947cb6469
sindharta <sindharta@gmail.com>
2020-07-08 19:58:19 +0900

    interim

---
+++ Runtime/Scripts/ImageLoader/ImageLoadController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace UnityEngine.StreamingImageSequence {
+
+
+internal class ImageLoadController  {
+
+    ImageLoadController GetInstance(int imageType) {
+        return m_imageLoadControllers[imageType];
+    } 
+
+//----------------------------------------------------------------------------------------------------------------------
+    internal void Queue(string imagePath, BackGroundTask task) {
+        
+        if (Application.isPlaying)        
+            UpdateManager.QueueBackGroundTask(task);
+        else {
+            
+        }
+        
+    }
+
+//----------------------------------------------------------------------------------------------------------------------
+
+    internal void UpdateInEditor() {
+        
+        
+    }
+
+//----------------------------------------------------------------------------------------------------------------------
+    private readonly Queue<PreviewImageLoadInfo> m_loadQueue = new Queue<PreviewImageLoadInfo>();
+
+    private readonly Dictionary<string, PreviewImageLoadInfo> m_loadDictionary =
+        new Dictionary<string, PreviewImageLoadInfo>();
+    
+//----------------------------------------------------------------------------------------------------------------------
+
+    private static readonly ImageLoadController[] m_imageLoadControllers 
+        = new ImageLoadController[StreamingImageSequenceConstants.MAX_IMAGE_TYPES];
+}
+
+} //end namespace
