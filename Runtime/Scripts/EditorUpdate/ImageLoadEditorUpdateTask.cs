94b534dec8c9c50806fe3ef3b206ed938c75e00b
sindharta <sindharta@gmail.com>
2020-07-09 16:55:28 +0900

    Use hash to prevent adding the same item to queue

--- Runtime/Scripts/EditorUpdate/ImageLoadEditorUpdateTask.cs
+++ Runtime/Scripts/EditorUpdate/ImageLoadEditorUpdateTask.cs
@@ -10,15 +10,17 @@ internal class ImageLoadEditorUpdateTask : ITask {
 
         if (m_requestedImageLoadBGTasks.Count <= 0)
             return;
-
+        
         //Don't push everything to ThreadManager
         const int BACKGROUND_TASKS_WAIT_THRESHOLD = 32;
-        int numBackGroundTasks = ThreadManager.GetNumBackGroundTasks();
+        int       numBackGroundTasks              = ThreadManager.GetNumBackGroundTasks();
         if (numBackGroundTasks >= BACKGROUND_TASKS_WAIT_THRESHOLD) {
             return;
         }
 
         BaseImageLoadBGTask task = m_requestedImageLoadBGTasks.Dequeue();
+        m_taskHashSet.Remove(task.GetImagePath());
+
         ThreadManager.QueueBackGroundTask(task);
     }
 
@@ -26,22 +28,33 @@ internal class ImageLoadEditorUpdateTask : ITask {
 //----------------------------------------------------------------------------------------------------------------------
 
     internal void RequestLoadImage(BaseImageLoadBGTask task) {
-
         //Clear old tasks
         int taskRequestFrame = task.GetRequestFrame();
         if (taskRequestFrame > m_latestFrame) {
             m_latestFrame = taskRequestFrame;
             m_requestedImageLoadBGTasks.Clear();
+            m_taskHashSet.Clear();
         }
-               
+
+    
+        string imagePath = task.GetImagePath();
+        if (m_taskHashSet.Contains(imagePath)) {
+            return;            
+        }
+                          
         m_requestedImageLoadBGTasks.Enqueue(task);
+        m_taskHashSet.Add(imagePath);
+            
+
     }
 
 
     
 //----------------------------------------------------------------------------------------------------------------------
     
-    readonly Queue<BaseImageLoadBGTask> m_requestedImageLoadBGTasks = new Queue<BaseImageLoadBGTask>(); 
+    readonly Queue<BaseImageLoadBGTask> m_requestedImageLoadBGTasks = new Queue<BaseImageLoadBGTask>();
+    readonly HashSet<string> m_taskHashSet = new HashSet<string>();
+    
     
     private int m_latestFrame = 0;
 
