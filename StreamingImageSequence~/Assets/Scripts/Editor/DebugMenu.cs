005e2664620ba3328b9eab8030d31dbea054ee87
sindharta <sindharta@gmail.com>
2020-07-10 21:19:47 +0900

    Add a debug script in the test project to show loaded textures
    (cherry picked from commit cc66459579e6e3943ba8a2328b8133d03d6b2eb3)

---
+++ StreamingImageSequence~/Assets/Scripts/Editor/DebugMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+public static class DebugMenu  {
+    
+[MenuItem("Debug/Find Loaded Textures")]
+private static void DebugTextures() {
+
+    Texture[] textures = Resources.FindObjectsOfTypeAll<Texture>();
+    StringBuilder sb = new StringBuilder();
+    sb.AppendLine($"Found {textures.Length} textures: ");
+    foreach (Texture tex in textures) {
+        sb.AppendLine($"    TexName: {tex.name} Dimension: ({tex.width},{tex.height})");
+    }
+    Debug.Log(sb.ToString());
+}
+    
+}
