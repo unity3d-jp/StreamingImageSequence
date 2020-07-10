27bbb30650ae37a166f1a14e7f1db0a91637c89c
sindharta <sindharta@gmail.com>
2020-07-10 22:02:16 +0900

    Add debug functions inside the test project
    (cherry picked from commit 19d6a7fe414bd9d4d6d70c62d4640867bf23cb29)

--- StreamingImageSequence~/Assets/Scripts/Editor/DebugMenu.cs
+++ StreamingImageSequence~/Assets/Scripts/Editor/DebugMenu.cs
@@ -4,16 +4,40 @@ using UnityEngine;
 
 public static class DebugMenu  {
     
-[MenuItem("Debug/Find Loaded Textures")]
-private static void DebugTextures() {
+    [MenuItem("Debug/Find Loaded Textures")]
+    private static void FindLoadedTextures() {
 
-    Texture[] textures = Resources.FindObjectsOfTypeAll<Texture>();
-    StringBuilder sb = new StringBuilder();
-    sb.AppendLine($"Found {textures.Length} textures: ");
-    foreach (Texture tex in textures) {
-        sb.AppendLine($"    TexName: {tex.name} Dimension: ({tex.width},{tex.height})");
+        Texture[] textures = Resources.FindObjectsOfTypeAll<Texture>();
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine($"Found {textures.Length} textures: ");
+        foreach (Texture tex in textures) {
+            sb.AppendLine($"    TexName: {tex.name} Dimension: ({tex.width},{tex.height})");
+        }
+        Debug.Log(sb.ToString());
     }
-    Debug.Log(sb.ToString());
-}
+
+//----------------------------------------------------------------------------------------------------------------------    
+    [MenuItem("Debug/Find Loaded Objects")]
+    private static void FindLoadedObjects() {
+
+        Object[] objects = Resources.FindObjectsOfTypeAll<Object>();
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine($"Found {objects.Length} objects: ");
+        foreach (Object obj in objects) {
+            sb.AppendLine($"    ObjectName: {obj.name} ({obj.GetType().ToString()}");
+        }
+        Debug.Log(sb.ToString());
+    }
     
+//----------------------------------------------------------------------------------------------------------------------    
+    [MenuItem("Debug/CollectGarbage")]
+    private static void CollectGarbage() {
+        System.GC.Collect();            
+    }
+
+//----------------------------------------------------------------------------------------------------------------------    
+    [MenuItem("Debug/UnloadUnusedAssets GC")]
+    private static void UnloadUnusedAssets() {
+        Resources.UnloadUnusedAssets();
+    }
 }
