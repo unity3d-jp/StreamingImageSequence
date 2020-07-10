99f57487bdb6b25b929fc42654bca2f09f2484d3
sindharta <sindharta@gmail.com>
2020-07-10 11:11:47 +0900

    Split MemoryUtilityTest to a different file

--- Plugins~/Tests/CommonLib/CommonLibTestMain.cpp
+++ Plugins~/Tests/CommonLib/CommonLibTestMain.cpp
@@ -1,47 +1,7 @@
 
 #include <gtest/gtest.h>
-#include <iostream>
-
-//CommonLib
-#include "CommonLib/MemoryUtility.h" 
 
 namespace StreamingImageSequencePluginTest {
-
-//----------------------------------------------------------------------------------------------------------------------
-
-TEST(CommonLib, GetTotalRAM) {
-    using namespace StreamingImageSequencePlugin;
-    const DWORDLONG totalRAM = MemoryUtility::GetTotalRAM();
-    ASSERT_GT(totalRAM, 0);
-}
-
-TEST(CommonLib, GetUsedRAM) {
-    using namespace StreamingImageSequencePlugin;
-    const DWORDLONG usedRAM = MemoryUtility::GetUsedRAM();
-    ASSERT_GT(usedRAM, 0);
-}
-
-TEST(CommonLib, GetAvailableRAM) {
-    using namespace StreamingImageSequencePlugin;
-    const DWORDLONG availableRAM = MemoryUtility::GetAvailableRAM();
-    ASSERT_GT(availableRAM, 0);
-}
-
-TEST(CommonLib, GetUsedRAMRatio) {
-    using namespace StreamingImageSequencePlugin;
-    const float usedRAMRatio = MemoryUtility::GetUsedRAMRatio();
-    ASSERT_GT(usedRAMRatio, 0.0f);
-    ASSERT_LT(usedRAMRatio, 1.0f);
-}
-
-TEST(CommonLib, GetAvailableRAMRatio) {
-    using namespace StreamingImageSequencePlugin;
-    const float availableRAMRatio = MemoryUtility::GetAvailableRAMRatio();
-    ASSERT_GT(availableRAMRatio, 0);
-    ASSERT_LT(availableRAMRatio, 1.0f);
-}
-
-//----------------------------------------------------------------------------------------------------------------------
 
 int main(int argc, char** argv) {
 
