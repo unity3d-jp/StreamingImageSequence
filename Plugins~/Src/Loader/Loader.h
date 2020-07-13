7580c6b448c6f5968316d567e2cfca09c32d3c7d
sindharta <sindharta@gmail.com>
2020-07-13 22:54:38 +0900

    Add an API to get used imaged memory

--- Plugins~/Src/Loader/Loader.h
+++ Plugins~/Src/Loader/Loader.h
@@ -35,6 +35,7 @@ extern "C"
 	LOADER_API uint32_t GetNumLoadedImages(const uint32_t imageType);
 
 	LOADER_API void  SetMaxImagesMemory(const uint32_t maxImageMemoryMB);
+	LOADER_API int GetUsedImagesMemory();
 
 
 	LOADER_API void  ResetPlugin();
