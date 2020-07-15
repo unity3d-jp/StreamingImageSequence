ca0a9185a2c3343d9e6dcc4266db3a493eeae1ee
sindharta <sindharta@gmail.com>
2020-07-15 15:40:12 +0900

    Add TimelineClipSISData script

---
+++ Runtime/Scripts/PlayableAssets/TimelineClipSISData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace UnityEngine.StreamingImageSequence {
+    
+[Serializable]
+internal class TimelineClipSISData {
+    [SerializeField] private List<PlayableFrame> m_playableFrames;
+}
+
+
+} //end namespace
+
+
+//A structure to store if we should use the image at a particular frame
