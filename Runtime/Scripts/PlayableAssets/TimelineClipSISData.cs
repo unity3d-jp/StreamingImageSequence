16c2003ac32c7ef24ebc60761857d5a3d0411cca
sindharta <sindharta@gmail.com>
2020-07-15 18:35:36 +0900

    interim, error compile

--- Runtime/Scripts/PlayableAssets/TimelineClipSISData.cs
+++ Runtime/Scripts/PlayableAssets/TimelineClipSISData.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEditor.Timeline;
 using UnityEngine.Assertions;
+using UnityEngine.Timeline;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
@@ -92,6 +93,78 @@ internal class TimelineClipSISData {
             }
         }
             
+            
+    }
+
+//----------------------------------------------------------------------------------------------------------------------    
+    
+    //Need to split the PlayableFrames which are currently shared by both this and m_clonedFromAsset
+    internal void TrySplitPlayableFrames(TimelineClipSISData otherSISData, double otherClipStart, double otherClipDuration) {
+
+        TimelineClip clip = m_playableAsset.GetBoundTimelineClip();
+        int numIdealFrames = StreamingImageSequencePlayableAsset.CalculateIdealNumPlayableFrames(clip);
+        
+        List<PlayableFrame> prevPlayableFrames = m_playableFrames;
+        m_playableFrames = new List<PlayableFrame>(numIdealFrames);
+        int prevNumPlayableFrames = prevPlayableFrames.Count;
+        
+        //Check if this clone is a pure duplicate                   
+        if (Math.Abs(clip.duration - otherClipDuration) < 0.0000001f) {
+            for (int i = 0; i < prevNumPlayableFrames; ++i) {
+                m_playableFrames.Add(null);
+                CreatePlayableFrame( i);
+                m_playableFrames[i].SetUsed(prevPlayableFrames[i].IsUsed());
+            }
+            return;
+        }
+
+        //Decide which one is on the left side after splitting
+        if (clip.start < otherClipStart) {
+            m_playableFrames.AddRange(prevPlayableFrames.GetRange(0,numIdealFrames));
+            m_clonedFromAsset.SplitPlayableFramesFromClonedAsset(numIdealFrames,prevPlayableFrames.Count - numIdealFrames);
+        } else {
+            int idx = prevPlayableFrames.Count - numIdealFrames;
+            m_playableFrames.AddRange(prevPlayableFrames.GetRange(idx, idx + numIdealFrames -1));
+            m_clonedFromAsset.SplitPlayableFramesFromClonedAsset(0,idx);
+        }
+        
+        //Reinitialize to assign the owner
+        double timePerFrame = CalculateTimePerFrame(m_boundTimelineClip);
+        for (int i = 0; i < numIdealFrames; ++i) {
+            m_playableFrames[i].Init(m_playableAsset, timePerFrame * i, m_useImageMarkerVisibility);
+        }
+        
+    }
+    
+    
+    //This is called by the cloned asset
+    void SplitPlayableFramesFromClonedAsset(int startIndex, int count) {
+
+        int numIdealFrames = CalculateIdealNumPlayableFrames(m_boundTimelineClip);
+        if (numIdealFrames != count) {
+            Debug.LogWarning("StreamingImageSequencePlayableAsset::ReassignPlayableFrames() Count: " + count
+                + " is not ideal: " + numIdealFrames                
+            );
+            return;
+        }
+
+        List<PlayableFrame> prevPlayableFrames = m_playableFrames;
+        if (startIndex + count > prevPlayableFrames.Count) {
+            Debug.LogWarning("StreamingImageSequencePlayableAsset::ReassignPlayableFrames() Invalid params. "
+                + " StartIndex: " + startIndex +  ", Count: " + count                
+            );
+            return;
+        }
+            
+        m_playableFrames = new List<PlayableFrame>(numIdealFrames);
+        m_playableFrames.AddRange(prevPlayableFrames.GetRange(startIndex,numIdealFrames));
+
+        double timePerFrame = CalculateTimePerFrame(m_boundTimelineClip);
+            
+        //Reinitialize to set the time
+        for (int i = 0; i < numIdealFrames; ++i) {
+            m_playableFrames[i].Init(m_playableAsset, timePerFrame * i, m_playableAsset.GetUseImageMarkerVisibility());
+        }
             
     }
     
