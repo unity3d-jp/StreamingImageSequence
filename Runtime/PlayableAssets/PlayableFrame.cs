a97ff52827e4030bac2cb8cc3cc1ba540fe446d3
sindharta <sindharta@gmail.com>
2020-07-20 15:49:27 +0900

    minor

--- Runtime/PlayableAssets/PlayableFrame.cs
+++ Runtime/PlayableAssets/PlayableFrame.cs
@@ -8,8 +8,8 @@ using UnityEngine.Timeline;
 
 namespace UnityEngine.StreamingImageSequence {
 
-//[TODO-sin: 2020-7-16]
-//Replaced by SISPlayableFrame. This is a dummy structure so that opening assets/scenes created using 
+//[TODO-sin: 2020-7-16] Replaced by SISPlayableFrame.
+//This is a dummy structure so that opening assets/scenes created using 
 //StreamingImageSequence up to 0.3.x don't produce any errors.
 
 [Serializable]
