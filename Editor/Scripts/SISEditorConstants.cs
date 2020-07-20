446cf4ebe5e74d8dbc0a282578abf60298e20838
sindharta <sindharta@gmail.com>
2020-07-20 22:05:43 +0900

    Add EditorConstants

---
+++ Editor/Scripts/SISEditorConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace UnityEditor.StreamingImageSequence {
+
+internal static class SISEditorConstants {
+
+    internal const string MAIN_USER_SETTINGS_PATH = "Packages/com.unity.streaming-image-sequence/Editor/UIElements/UserSettings/UserSettings_Main";
+
+    internal const string USER_SETTINGS_STYLE_PATH = "Packages/com.unity.streaming-image-sequence/Editor/UIElements/UserSettings/UserSettings_Style";
+
+
+}
+
+} //end namespace
