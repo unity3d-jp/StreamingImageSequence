8ad200030eb5f3ee3768d69e38bca7a7bdee12f5
sindharta <sindharta@gmail.com>
2020-07-20 22:21:00 +0900

    Add a test for testing UserSettings

---
+++ Tests/Editor/UserSettingsTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Unity.AnimeToolbox.Editor;
+using UnityEngine.UIElements;
+
+namespace UnityEditor.StreamingImageSequence.Tests {
+
+internal class UserSettingsTest {
+
+    [Test]
+    public void VerifyUIElementsResources() {
+        
+        VisualTreeAsset main = UIElementsEditorUtility.LoadVisualTreeAsset(SISEditorConstants.MAIN_USER_SETTINGS_PATH);
+        Assert.IsNotNull(main);
+			
+        //Style
+        StyleSheet styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>(SISEditorConstants.USER_SETTINGS_STYLE_PATH + ".uss");
+        Assert.IsNotNull(styleSheet);
+        
+    }
+}
+
+} //end namespace
