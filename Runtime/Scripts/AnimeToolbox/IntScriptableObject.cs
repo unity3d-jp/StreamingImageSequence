e7b2164e579e9cbcb0d12b51781fc270b84e9769
sindharta <sindharta@gmail.com>
2020-07-21 18:40:44 +0900

    Add IntScriptableObject

---
+++ Runtime/Scripts/AnimeToolbox/IntScriptableObject.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Unity.AnimeToolbox {
+
+//[TODO-sin: 2020-7-21] Move this to com.unity.anime-toolbox
+/// <summary>
+/// A ScriptableObject which contains only an integer
+/// </summary>
+public class IntScriptableObject : ScriptableObject  {
+    public int Value = 0;
+}
+   
+}
