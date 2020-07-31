8fb3ccd18c0367ef198003b4fc0cd1fd9e856935
sindharta <sindharta@gmail.com>
2020-07-31 10:41:58 +0900

    minor

--- Editor/Utilities/InspectorUtility.cs
+++ Editor/Utilities/InspectorUtility.cs
@@ -8,22 +8,19 @@ namespace UnityEditor.StreamingImageSequence {
 internal static class InspectorUtility {    
     internal static void ShowFrameMarkersGUI(BaseTimelineClipSISDataPlayableAsset timelineClipSISDataPlayableAsset) {        
 
-        //Image markers
-        if (TimelineEditor.selectedClip.asset == timelineClipSISDataPlayableAsset) {
-            TimelineClipSISData timelineClipSISData = timelineClipSISDataPlayableAsset.GetBoundTimelineClipSISData();
-            Assert.IsNotNull(timelineClipSISData);
-                    
-            GUILayout.Space(15);
-            bool prevMarkerVisibility = timelineClipSISData.AreFrameMarkersVisible();
-            bool markerVisibility     = GUILayout.Toggle(prevMarkerVisibility, "Show FrameMarkers");
-            if (markerVisibility != prevMarkerVisibility) {
-                timelineClipSISData.ShowFrameMarkers(markerVisibility);
-            }
+        TimelineClipSISData timelineClipSISData = timelineClipSISDataPlayableAsset.GetBoundTimelineClipSISData();
+        Assert.IsNotNull(timelineClipSISData);
                 
-                
-            if (GUILayout.Button("Reset FrameMarkers")) {
-                timelineClipSISDataPlayableAsset.ResetPlayableFrames();
-            }
+        GUILayout.Space(15);
+        bool prevMarkerVisibility = timelineClipSISData.AreFrameMarkersVisible();
+        bool markerVisibility     = GUILayout.Toggle(prevMarkerVisibility, "Show FrameMarkers");
+        if (markerVisibility != prevMarkerVisibility) {
+            timelineClipSISData.ShowFrameMarkers(markerVisibility);
+        }
+            
+            
+        if (GUILayout.Button("Reset FrameMarkers")) {
+            timelineClipSISDataPlayableAsset.ResetPlayableFrames();
         }
             
     }
