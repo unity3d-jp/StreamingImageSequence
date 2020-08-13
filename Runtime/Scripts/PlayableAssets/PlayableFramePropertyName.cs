989342c785157c4e0cd99bd000593787657e2198
Sindharta Tanuwijaya <>
2020-08-13 14:24:15 +0900

    Add PlayableFramePropertyName

---
+++ Runtime/Scripts/PlayableAssets/PlayableFramePropertyName.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnityEngine.StreamingImageSequence {
+    
+[Serializable]
+internal static class PlayableFramePropertyName {
+    internal const string USED = "Used";
+    internal const string LOCKED = "Locked";    
+}
+
+} //end namespace
+
