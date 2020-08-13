c22db374e038287a4042a4ee155323d8d6796459
Sindharta Tanuwijaya <>
2020-08-13 16:07:18 +0900

    Add PlayableFrameExtension class (Will have compile errors at this point)

---
+++ Runtime/Scripts/PlayableAssets/PlayableFrameExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace UnityEngine.StreamingImageSequence {
+    
+[Serializable]
+internal static class PlayableFrameExtension {
+
+    internal static void SetUsed(this SISPlayableFrame playableFrame, bool used) {
+        playableFrame.SetBoolProperty(PlayableFramePropertyName.USED, used);
+    }
+
+    internal static bool IsUsed(this SISPlayableFrame playableFrame) {
+        return null != playableFrame && playableFrame.GetBoolProperty(PlayableFramePropertyName.USED);
+    }
+    
+    internal static void SetLocked(this SISPlayableFrame playableFrame, bool used) {
+        playableFrame.SetBoolProperty(PlayableFramePropertyName.LOCKED, used);
+    }
+
+    internal static bool IsLocked(this SISPlayableFrame playableFrame) {
+        return null != playableFrame && playableFrame.GetBoolProperty(PlayableFramePropertyName.LOCKED);
+    }
+    
+}
+
+} //end namespace
+
