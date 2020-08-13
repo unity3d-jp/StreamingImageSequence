2685eae8168c7d4e05298c74c87990d0a5250419
Sindharta Tanuwijaya <>
2020-08-13 12:55:59 +0900

    Load FrameMarkerLock texture

--- Editor/EditorTextures.cs
+++ Editor/EditorTextures.cs
@@ -1,20 +1,28 @@
-﻿using UnityEngine;
+﻿using System.IO;
+using UnityEngine;
 
 namespace UnityEditor.StreamingImageSequence {
 
 internal static class EditorTextures {
 
     internal static Texture GetCheckedTexture() {
-        if (null == m_checkedTexture) {
+        if (!m_initialized) {
             LoadTextures();
         }
         return m_checkedTexture;
+    }
+
+    internal static Texture GetLockTexture() {
+        if (!m_initialized) {
+            LoadTextures();
+        }
+        return m_lockTexture;
     }
     
 //----------------------------------------------------------------------------------------------------------------------
 
     [InitializeOnLoadMethod]
-    static void OnLoad() {
+    static void EditorTextures_OnLoad() {
         LoadTextures();
     }
 
@@ -22,13 +30,22 @@ internal static class EditorTextures {
 //----------------------------------------------------------------------------------------------------------------------
 
     static void LoadTextures() {
-        const string FULL_PATH = "Packages/com.unity.streaming-image-sequence/Editor/Textures/Checked.png";
-        m_checkedTexture = AssetDatabase.LoadAssetAtPath<Texture>(FULL_PATH);
+        const string CHECKED_TEX_FULL_PATH = "Packages/com.unity.streaming-image-sequence/Editor/Textures/Checked.png";
+        m_checkedTexture = AssetDatabase.LoadAssetAtPath<Texture>(CHECKED_TEX_FULL_PATH);
+        
+        const string STYLESHEET_IMAGE_PATH = "Packages/com.unity.streaming-image-sequence/Editor/StyleSheets/Images";
+        string skin = EditorGUIUtility.isProSkin ? "DarkSkin" : "LightSkin";
+        string lockTexFullPath = Path.Combine(STYLESHEET_IMAGE_PATH, skin, "FrameMarkerLock.png");
+        m_lockTexture = AssetDatabase.LoadAssetAtPath<Texture>(lockTexFullPath);
+
+        m_initialized = true;
     }
     
 //----------------------------------------------------------------------------------------------------------------------
 
+    private static bool m_initialized = false;
     private static Texture m_checkedTexture;
+    private static Texture m_lockTexture;
 
 }
 
