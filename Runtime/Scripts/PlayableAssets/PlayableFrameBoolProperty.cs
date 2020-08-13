10d740cf1f2370b5265b0a2140f6a73bc7b42ca8
Sindharta Tanuwijaya <>
2020-08-13 15:46:50 +0900

    Change PlayableFrameProperty to non-generics because generics is not serializable.

--- Runtime/Scripts/PlayableAssets/PlayableFrameBoolProperty.cs
+++ Runtime/Scripts/PlayableAssets/PlayableFrameBoolProperty.cs
@@ -3,21 +3,22 @@
 namespace UnityEngine.StreamingImageSequence {
     
 [Serializable]
-internal class PlayableFrameProperty<T> where T: struct{
+internal class PlayableFrameBoolProperty{
     
-    PlayableFrameProperty(string name, T val) {
-        m_propertyName = name;
+    internal PlayableFrameBoolProperty(string name, bool val) {
+        m_propertyName  = name;
         m_propertyValue = val;
     }
 //----------------------------------------------------------------------------------------------------------------------
 
     internal string GetName() { return m_propertyName; }
-    internal T GetValue() { return m_propertyValue;}
+    internal bool GetValue() { return m_propertyValue;}
+
 //----------------------------------------------------------------------------------------------------------------------
     
     
-    private string m_propertyName = null;
-    private T m_propertyValue = default(T);
+    [SerializeField] private string m_propertyName = null;
+    [SerializeField] private bool m_propertyValue = false;
     
 }
 
