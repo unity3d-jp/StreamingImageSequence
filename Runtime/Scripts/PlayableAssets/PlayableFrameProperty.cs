c76187ab489c35ea72fcb77cbadf5e070f2c3fb4
Sindharta Tanuwijaya <>
2020-08-13 14:19:25 +0900

    Add PlayableFrameProperty to store property values for SISPlayableFrame

---
+++ Runtime/Scripts/PlayableAssets/PlayableFrameProperty.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace UnityEngine.StreamingImageSequence {
+    
+[Serializable]
+internal class PlayableFrameProperty<T> where T: struct{
+    
+    PlayableFrameProperty(string name, T val) {
+        m_propertyName = name;
+        m_propertyValue = val;
+    }
+//----------------------------------------------------------------------------------------------------------------------
+
+    internal string GetName() { return m_propertyName; }
+    internal T GetValue() { return m_propertyValue;}
+//----------------------------------------------------------------------------------------------------------------------
+    
+    
+    private string m_propertyName = null;
+    private T m_propertyValue = default(T);
+    
+}
+
+} //end namespace
+
