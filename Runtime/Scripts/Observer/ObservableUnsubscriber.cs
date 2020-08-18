106393901cc6fdca3300e317e1fda14743ea9d8d
Sindharta Tanuwijaya <>
2020-08-18 16:37:07 +0900

    Create initial Observable class

---
+++ Runtime/Scripts/Observer/ObservableUnsubscriber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnityEngine.StreamingImageSequence
+{
+
+//For subscribing a particular observer from where it has been registered into
+internal class ObservableUnsubscriber<T> : IDisposable {
+
+    internal ObservableUnsubscriber(List<IObserver<T>> allObservers, IObserver<T> observer) {
+        this.m_allObservers = allObservers;
+        this.m_observer  = observer;
+    }
+
+    public void Dispose() {
+        if (m_allObservers.Contains(m_observer))
+            m_allObservers.Remove(m_observer);
+    }
+    
+    private readonly List<IObserver<T>> m_allObservers;
+    private readonly IObserver<T>       m_observer;
+}
+} //end namespace
---
+++ Runtime/Scripts/Observer/PlayableAssetFolderBroadcaster.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnityEngine.StreamingImageSequence
+{
+    
+internal class PlayableAssetFolderBroadcaster : IObservable<string> {
+
+    internal static PlayableAssetFolderBroadcaster GetInstance() {
+        return m_instance;
+    }    
+//----------------------------------------------------------------------------------------------------------------------    
+        
+    private PlayableAssetFolderBroadcaster() {
+        m_observers = new List<IObserver<string>>();
+    }
+    
+    public IDisposable Subscribe(IObserver<string> observer) {
+        if (! m_observers.Contains(observer)) {
+            m_observers.Add(observer);
+        }
+        return new ObservableUnsubscriber<string>(m_observers, observer);
+    }
+    
+//----------------------------------------------------------------------------------------------------------------------    
+    private readonly List<IObserver<string>> m_observers;
+    
+    private static readonly PlayableAssetFolderBroadcaster m_instance = new PlayableAssetFolderBroadcaster();
+
+}
+
+} //end namespace
