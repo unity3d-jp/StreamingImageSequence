aadd65dd24ef7cb86bc27037e0cd87e96dd76352
Sindharta Tanuwijaya <>
2020-08-18 20:13:25 +0900

    Add BitUtility class

---
+++ Runtime/Scripts/Utilities/BitUtility.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.Assertions;
+
+namespace UnityEngine.StreamingImageSequence {
+
+internal static class BitUtility {
+        
+//----------------------------------------------------------------------------------------------------------------------    
+    internal static void ConvertToByte(long number, ref byte[] bytes) {
+        const int NUM_LONG_BYTES = sizeof(long);
+        Assert.AreEqual(NUM_LONG_BYTES, bytes.Length);
+        for (int i=0;i<NUM_LONG_BYTES;++i) {
+            bytes[i] = (byte)(number >> (8*i) & 0xFF);
+        }
+    }
+}
+
+} //end namespace
