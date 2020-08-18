ede6888f6b0aa0fbfb8325e5708cb1530d62df70
Sindharta Tanuwijaya <>
2020-08-18 19:31:42 +0900

    Simplify

--- Runtime/Scripts/Utilities/PathUtility.cs
+++ Runtime/Scripts/Utilities/PathUtility.cs
@@ -5,16 +5,11 @@ namespace UnityEngine.StreamingImageSequence {
 internal static class PathUtility {
     
     internal static string GetPath(string folder, string fileName) {
-        string filePath = null;
-            
-        if (!string.IsNullOrEmpty(folder)) {
-            filePath = Path.Combine(folder, fileName);
-        } else {
-            filePath = fileName;
-        }
-
+        string filePath = !string.IsNullOrEmpty(folder) ? Path.Combine(folder, fileName) : fileName;
         return filePath;
     }
+    
+    
 }
 
 }
