c88e52a550ecf858f15324fdf66ae1f923eeb705
Sindharta Tanuwijaya <>
2020-08-18 17:02:54 +0900

    Add Unsubscribe() and Notify() to FolderContentsChangedNotifier

--- Runtime/Scripts/Observer/FolderContentsChangedNotifier.cs
+++ Runtime/Scripts/Observer/FolderContentsChangedNotifier.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+#endif
 
 namespace UnityEngine.StreamingImageSequence
 {
     
 internal sealed class FolderContentsChangedNotifier : IObservable<string> {
+
+#if UNITY_EDITOR
+    [InitializeOnLoadMethod]
+    static void FolderContentsChangedNotifier_OnEditorLoad() {        
+        EditorSceneManager.sceneClosed += FolderContentsChangedNotifier_OnSceneClosed;
+    }
+    
+    static void FolderContentsChangedNotifier_OnSceneClosed(Scene scene) {
+        FolderContentsChangedNotifier.GetInstance().UnsubscribeAll();
+    }
+#endif
+
+//----------------------------------------------------------------------------------------------------------------------    
 
     internal static FolderContentsChangedNotifier GetInstance() {
         return m_instance;
@@ -12,7 +31,7 @@ internal sealed class FolderContentsChangedNotifier : IObservable<string> {
 //----------------------------------------------------------------------------------------------------------------------    
         
     private FolderContentsChangedNotifier() {
-        m_observers = new List<IObserver<string>>();
+        m_observers = new HashSet<IObserver<string>>();
     }
     
     public IDisposable Subscribe(IObserver<string> observer) {
@@ -22,9 +41,30 @@ internal sealed class FolderContentsChangedNotifier : IObservable<string> {
         return new ObservableUnsubscriber<string>(m_observers, observer);
     }
 
+    internal bool Unsubscribe(IObserver<string> observer) {
+        if (! m_observers.Contains(observer)) {
+            return false;
+        }
+
+        m_observers.Remove(observer);
+        return true;
+    }
+
+    private void UnsubscribeAll() {
+        m_observers.Clear();
+    }
     
 //----------------------------------------------------------------------------------------------------------------------    
-    private readonly List<IObserver<string>> m_observers;
+    
+    internal void Notify(string folder) {
+        foreach (IObserver<string> observer in m_observers) {
+            observer.OnNext(folder);
+        }
+        
+    }
+    
+//----------------------------------------------------------------------------------------------------------------------    
+    private readonly HashSet<IObserver<string>> m_observers;
     
     private static readonly FolderContentsChangedNotifier m_instance = new FolderContentsChangedNotifier();
 
