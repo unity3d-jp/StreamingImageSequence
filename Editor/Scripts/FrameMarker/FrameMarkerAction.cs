6d8e42d0c97ba2da2c7952319faabb6e569bd6a5
sindharta <sindharta@gmail.com>
2020-08-21 17:05:28 +0900

    Add pop-up menu for FrameMarker (Lock and Edit)

---
+++ Editor/Scripts/FrameMarker/FrameMarkerAction.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using UnityEditor.Timeline.Actions;
+using UnityEngine.StreamingImageSequence;
+using UnityEngine.Timeline;
+
+namespace UnityEditor.StreamingImageSequence {
+
+[MenuEntry("Lock and Edit")]
+class LockAndEditFrameMarkerAction : MarkerAction
+{
+    ///<inheritdoc />
+    public override bool Execute(IEnumerable<IMarker> markers) {
+        
+        foreach (IMarker marker in markers) {
+            FrameMarker frameMarker = marker as FrameMarker;
+            if (null == frameMarker)
+                return false;
+            
+            SISPlayableFrame playableFrame = frameMarker.GetOwner();
+            RenderCachePlayableAsset playableAsset = playableFrame.GetTimelineClipAsset<RenderCachePlayableAsset>();            
+            if (null == playableAsset)
+                return false;
+            
+            FrameMarkerInspector.LockAndEditPlayableFrame(playableFrame, playableAsset);
+            
+        }
+
+        return true;
+    }
+
+//----------------------------------------------------------------------------------------------------------------------    
+    ///<inheritdoc />
+    public override ActionValidity Validate(IEnumerable<IMarker> markers) {        
+        foreach(IMarker marker in markers) {
+            FrameMarker frameMarker = marker as FrameMarker;
+            if (null == frameMarker)
+                return ActionValidity.NotApplicable;
+            
+            
+            SISPlayableFrame playableFrame = frameMarker.GetOwner();
+            RenderCachePlayableAsset playableAsset = playableFrame.GetTimelineClipAsset<RenderCachePlayableAsset>();            
+            if (null == playableAsset)
+                return ActionValidity.NotApplicable;            
+
+        }
+        return ActionValidity.Valid;
+    }
+    
+}
+
+
+} //end namespace
+
