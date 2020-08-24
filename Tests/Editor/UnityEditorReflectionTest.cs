09eb2d63f41395363b7aec060e097e69464bbcf6
sindharta <sindharta@gmail.com>
2020-08-24 23:14:04 +0900

    Add a unit test to verify reflected methods

---
+++ Tests/Editor/UnityEditorReflectionTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace UnityEditor.StreamingImageSequence.Tests {
+
+internal class UnityEditorReflectionTest {
+
+    [Test]
+    public void VerifyReflectedMethods() {
+        
+        Assert.IsNotNull(UnityEditorReflection.SCROLLABLE_TEXT_AREA_METHOD);
+        
+
+    }
+
+
+}
+
+} //end namespace
