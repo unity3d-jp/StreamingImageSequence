c99dcedd36a854215b514b4fcbabdd4daa647e2d
sindharta <sindharta@gmail.com>
2020-08-24 23:08:03 +0900

    Add a new class to store methods/classes that use reflection

---
+++ Editor/Scripts/UnityEditorReflection.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace UnityEditor.StreamingImageSequence {
+
+internal static class UnityEditorReflection {
+    
+    internal static readonly MethodInfo SCROLLABLE_TEXT_AREA_METHOD 
+        = typeof(EditorGUI).GetMethod("ScrollableTextAreaInternal", BindingFlags.Static | BindingFlags.NonPublic);
+
+}
+
+} //end namespace
