7702c585ab894181802c83b40165148e1f6a4f71
sindharta <sindharta@gmail.com>
2020-08-25 17:18:17 +0900

    Add initial BaseSISTrack and SISTrackCaps

---
+++ Runtime/Scripts/BaseSISTrack.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine.Timeline;
+
+namespace UnityEngine.StreamingImageSequence
+{
+    
+internal abstract class BaseSISTrack : TrackAsset {
+    protected virtual SISTrackCaps GetCapsV() { return SISTrackCaps.NONE; }    
+}
+
+} //end namespace
