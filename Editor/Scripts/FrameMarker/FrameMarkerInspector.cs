6786ccf2faa3d1548612d03d5332d57c9f64f86f
sindharta <sindharta@gmail.com>
2020-08-25 14:16:16 +0900

    null check

--- Editor/Scripts/FrameMarker/FrameMarkerInspector.cs
+++ Editor/Scripts/FrameMarker/FrameMarkerInspector.cs
@@ -158,7 +158,7 @@ internal class FrameMarkerInspector: Editor {
         }; 
         object userNoteObj = UnityEditorReflection.SCROLLABLE_TEXT_AREA_METHOD.Invoke(null,methodParams);
         m_noteScroll = (Vector2) (methodParams[2]);
-        string userNote = userNoteObj.ToString();
+        string userNote = userNoteObj?.ToString();
         
         if (userNote != prevNote) {
             foreach (FrameMarker frameMarker in m_assets) {
