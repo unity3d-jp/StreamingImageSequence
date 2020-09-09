4108157b72c47a6afb70ba8b0ebd3d7a8fed51d0
sindharta <sindharta@gmail.com>
2020-09-09 17:29:50 +0900

    Add WatchedFileInfo

---
+++ Runtime/Scripts/WatchedFileInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UnityEngine.StreamingImageSequence {
+
+[Serializable]
+internal class WatchedFileInfo {
+    internal WatchedFileInfo(string name, long size) {
+        m_name = name;
+        m_size = size;
+
+    }
+//----------------------------------------------------------------------------------------------------------------------    
+    
+    internal string GetName() { return m_name; }
+    internal long GetSize() { return m_size; }
+    
+//----------------------------------------------------------------------------------------------------------------------
+    
+    
+    
+//----------------------------------------------------------------------------------------------------------------------    
+    
+    [SerializeField] private string m_name;
+    [SerializeField] private long   m_size;
+}
+} //end namespace
+
+
