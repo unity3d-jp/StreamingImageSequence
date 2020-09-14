0856d2f59ba9f17d5d0c2dc7441be3203fe0f414
sindharta <sindharta@gmail.com>
2020-09-14 16:28:10 +0900

    Moving StreamingImageSequence folder to Scripts/SIS and Scripts/Preview

---
+++ Editor/Scripts/SIS/StreamingImageSequenceTrackEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor.Timeline;
+using UnityEngine;
+using UnityEngine.StreamingImageSequence;
+using UnityEngine.Timeline;
+
+namespace UnityEditor.StreamingImageSequence {
+
+[CustomTimelineEditor(typeof(StreamingImageSequenceTrack))]
+internal class StreamingImageSequenceTrackEditor : TrackEditor {
+    public override TrackDrawOptions GetTrackOptions(TrackAsset track, Object binding) {
+        TrackDrawOptions options = base.GetTrackOptions(track, binding);
+        options.errorText = null;
+        
+        return options;
+    }
+}
+
+}
