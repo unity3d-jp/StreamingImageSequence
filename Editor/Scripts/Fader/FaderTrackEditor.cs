251a64f676f800cad3b233913b620ec432d5d4b6
sindharta <sindharta@gmail.com>
2020-09-14 16:28:46 +0900

    Moving Editor/Fader to Editor/Scripts/Fader

---
+++ Editor/Scripts/Fader/FaderTrackEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor.Timeline;
+using UnityEngine;
+using UnityEngine.StreamingImageSequence;
+using UnityEngine.Timeline;
+
+namespace UnityEditor.StreamingImageSequence {
+
+[CustomTimelineEditor(typeof(FaderTrack))]
+internal class FaderTrackEditor : TrackEditor {
+    public override TrackDrawOptions GetTrackOptions(TrackAsset track, Object binding) {
+        TrackDrawOptions options = base.GetTrackOptions(track, binding);
+        options.errorText = null;
+        
+        return options;
+    }
+}
+
+}
