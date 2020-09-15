cba9a6ac009ea4f97eb372648c1b913517eb87b3
sindharta <sindharta@gmail.com>
2020-09-15 16:57:42 +0900

    Open internals to compositor

--- Runtime/AssemblyInfo.cs
+++ Runtime/AssemblyInfo.cs
@@ -2,5 +2,6 @@
 [assembly: InternalsVisibleTo("Unity.StreamingImageSequence.Editor")]
 [assembly: InternalsVisibleTo("Unity.StreamingImageSequence.Tests")]
 [assembly: InternalsVisibleTo("Unity.StreamingImageSequence.EditorTests")]
+[assembly: InternalsVisibleTo("com.unity.compositor.runtime")]
 
 
