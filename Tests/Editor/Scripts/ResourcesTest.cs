398f79db01e805b106409c79734dfeb4fa057f6d
sindharta <sindharta@gmail.com>
2020-10-07 15:16:05 +0900

    Add a test to make sure the shader exists.

---
+++ Tests/Editor/Scripts/ResourcesTest.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using NUnit.Framework;
+using Unity.StreamingImageSequence.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace Unity.StreamingImageSequence.EditorTests {
+
+internal class ResourcesTest {
+    
+    [Test]
+    public void VerifyShaders() {
+
+        string path = SISEditorConstants.TRANSPARENT_BG_COLOR_SHADER_PATH;
+        Assert.IsTrue(File.Exists(path));
+        Shader shader = AssetDatabase.LoadAssetAtPath<Shader>(path);
+        Assert.IsNotNull(shader);        
+
+    }
+
+
+}
+
+} //end namespace
