b0cd0371d1ff14f2d827c4541bb5901fabdd4cd3
sindharta <sindharta@gmail.com>
2020-10-07 14:30:54 +0900

    Add Texture2DExtensions

---
+++ Runtime/Scripts/Extensions/Texture2DExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Unity.StreamingImageSequence {
+
+//[TODO-sin: 2020-10-7] Move to anime-toolbox
+internal static class Texture2DExtensions {
+    public static void SetPixelsWithColor(this Texture2D tex, Color color) {
+        
+        Color[] pixels =  tex.GetPixels();
+ 
+        for(var i = 0; i < pixels.Length; ++i) {
+            pixels[i] = color;
+        }  
+        tex.SetPixels( pixels );  
+        tex.Apply();                
+    }    
+}
+
+} //end namespace
