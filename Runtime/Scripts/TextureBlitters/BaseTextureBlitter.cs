97d370029e9ffc2158ed0fa2c32ec890125d9c49
sindharta <sindharta@gmail.com>
2020-10-12 12:00:32 +0900

    Add the initial version of BaseTextureBlitter

---
+++ Runtime/Scripts/TextureBlitters/BaseTextureBlitter.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace Unity.StreamingImageSequence {
+
+[RequireComponent(typeof(Camera))]
+internal abstract class BaseTextureBlitter : MonoBehaviour {    
+    
+    void Awake() {
+        m_camera = GetComponent<Camera>();
+        
+        //Render nothing
+        m_camera.clearFlags  = CameraClearFlags.Depth;
+        m_camera.cullingMask = 0;
+
+        AwakeInternalV();
+    }
+
+    protected abstract void AwakeInternalV();
+    
+//----------------------------------------------------------------------------------------------------------------------    
+
+    protected void BlitTexture(RenderTexture source, RenderTexture destination) {
+        if (null == m_texture) 
+            return;
+
+        if (null == m_blitMaterial) {
+            Graphics.Blit(m_texture, destination);
+            return;
+        }
+        
+        Graphics.Blit(m_texture, destination, m_blitMaterial);
+        
+    }
+
+//----------------------------------------------------------------------------------------------------------------------    
+
+    internal void SetTexture(Texture tex) { m_texture = tex; }
+    internal void SetBlitMaterial(Material blitMat) { m_blitMaterial = blitMat; }
+    internal void SetCameraDepth(int depth) { m_camera.depth = depth; }
+    
+//----------------------------------------------------------------------------------------------------------------------    
+
+    [SerializeField] private Texture  m_texture;    
+    [SerializeField] Material m_blitMaterial = null;
+    
+    private Camera m_camera;
+}
+
+} //end namespace
