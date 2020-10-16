a78992284010c384cfcf6258a99943626ad7ec6f
sindharta <sindharta@gmail.com>
2020-10-16 21:15:12 +0900

    invert ok and not ok check

--- Plugins~/Src/StreamingImageSequence/Win/LoaderWin.cpp
+++ Plugins~/Src/StreamingImageSequence/Win/LoaderWin.cpp
@@ -47,47 +47,47 @@ void LoadPNGFileAndAlloc(const strType& imagePath, const uint32_t imageType,
 		static_cast<int>(imagePath.length()),
 		wlocal,
 		size);
-	Gdiplus::Bitmap*    pBitmap = Gdiplus::Bitmap::FromFile(wlocal);
+	Gdiplus::Bitmap* bitmap = Gdiplus::Bitmap::FromFile(wlocal);
 
 # endif
 	Gdiplus::Status status = Gdiplus::FileNotFound;
-	if (pBitmap ) {
-		status = pBitmap->GetLastStatus();
+	if (bitmap ) {
+		status = bitmap->GetLastStatus();
 	}
 
-	if (status == Gdiplus::Ok)	{
-		const u32 width = pBitmap->GetWidth();
-		const u32 height = pBitmap->GetHeight();
-		const uint32_t dataSize = sizeof(u32) * width * height;
-
-		const ImageData* imageData = imageCatalog->AllocateImage(imagePath, imageType, width, height);
-		if (nullptr == imageData) {
-			imageCatalog->SetImageStatus(imagePath, imageType,READ_STATUS_OUT_OF_MEMORY);
-			return;
-		}
-
-		u8* pBuffer = imageData->RawData;
-		ASSERT(pBuffer !=nullptr);
-		u32* pImage = (u32*)pBuffer;
-
-		Gdiplus::BitmapData bitmapData;
-		pBitmap->LockBits(&Gdiplus::Rect(0, 0, width, height), Gdiplus::ImageLockModeWrite, PixelFormat32bppARGB, &bitmapData);
-		u32 *pRawBitmapOrig = (u32*)bitmapData.Scan0;   // for easy access and indexing
-		for (u32 yy = 0; yy < height; yy++) {
-			memcpy(&pImage[yy*width], &pRawBitmapOrig[(height - 1 - yy) * bitmapData.Stride / 4], width*sizeof(u32));
-
-		}
-
-		//memcpy(pImage, bitmapData.Scan0, width*height*sizeof(UINT32));
-		pBitmap->UnlockBits(&bitmapData);
-		delete pBitmap;
-
-		imageCatalog->SetImageStatus(imagePath, imageType,READ_STATUS_SUCCESS);
-		imageCatalog->SetImageFormat(imagePath, imageType,IMAGE_FORMAT_BGRA32);
-
-	} else {
+	if (Gdiplus::Ok!=status)	{
 		imageCatalog->SetImageStatus(imagePath, imageType,READ_STATUS_FAIL);
+		return;
 	}
+
+	const u32 width = bitmap->GetWidth();
+	const u32 height = bitmap->GetHeight();
+	const uint32_t dataSize = sizeof(u32) * width * height;
+
+	const ImageData* imageData = imageCatalog->AllocateImage(imagePath, imageType, width, height);
+	if (nullptr == imageData) {
+		imageCatalog->SetImageStatus(imagePath, imageType,READ_STATUS_OUT_OF_MEMORY);
+		return;
+	}
+
+	u8* pBuffer = imageData->RawData;
+	ASSERT(pBuffer !=nullptr);
+	u32* pImage = (u32*)pBuffer;
+
+	Gdiplus::BitmapData bitmapData;
+	bitmap->LockBits(&Gdiplus::Rect(0, 0, width, height), Gdiplus::ImageLockModeWrite, PixelFormat32bppARGB, &bitmapData);
+	u32 *pRawBitmapOrig = (u32*)bitmapData.Scan0;   // for easy access and indexing
+	for (u32 yy = 0; yy < height; yy++) {
+		memcpy(&pImage[yy*width], &pRawBitmapOrig[(height - 1 - yy) * bitmapData.Stride / 4], width*sizeof(u32));
+
+	}
+
+	//memcpy(pImage, bitmapData.Scan0, width*height*sizeof(UINT32));
+	bitmap->UnlockBits(&bitmapData);
+	delete bitmap;
+
+	imageCatalog->SetImageStatus(imagePath, imageType,READ_STATUS_SUCCESS);
+	imageCatalog->SetImageFormat(imagePath, imageType,IMAGE_FORMAT_BGRA32);
 
 }
 
