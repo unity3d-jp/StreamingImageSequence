1a752da789792943d88b5da43d070b7c9e90466f
sindharta <sindharta@gmail.com>
2020-10-16 21:20:30 +0900

    simplify indexing

--- Plugins~/Src/StreamingImageSequence/Win/LoaderWin.cpp
+++ Plugins~/Src/StreamingImageSequence/Win/LoaderWin.cpp
@@ -78,8 +78,12 @@ void LoadPNGFileAndAlloc(const strType& imagePath, const uint32_t imageType,
 	Gdiplus::BitmapData bitmapData;
 	bitmap->LockBits(&Gdiplus::Rect(0, 0, width, height), Gdiplus::ImageLockModeWrite, PixelFormat32bppARGB, &bitmapData);
 	u32 *pRawBitmapOrig = (u32*)bitmapData.Scan0;   // for easy access and indexing
+	const u32 heightMinusOneMulWidth = (height - 1) * width;
+	const u32 memSizePerRow = width * sizeof(u32);
 	for (u32 yy = 0; yy < height; yy++) {
-		memcpy(&pImage[yy*width], &pRawBitmapOrig[(height - 1 - yy) * bitmapData.Stride / 4], width*sizeof(u32));
+		const u32 destIndex = yy * width;
+		const u32 srcIndex = heightMinusOneMulWidth - destIndex; //From: (height - 1 - yy) * width;
+		memcpy(&pImage[destIndex], &pRawBitmapOrig[srcIndex], memSizePerRow);
 
 	}
 
