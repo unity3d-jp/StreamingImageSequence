9a641536e2f463ef65d434284480c8e851e48111
sindharta <sindharta@gmail.com>
2020-10-22 14:35:31 +0900

    Move the code to initialize maxMemorySlider to a function

--- Editor/Scripts/UserSettings/SISUserSettingsProvider.cs
+++ Editor/Scripts/UserSettings/SISUserSettingsProvider.cs
@@ -24,7 +24,6 @@ internal class SISUserSettingsProvider : SettingsProvider {
         //activateHandler is called when the user clicks on the Settings item in the Settings window.
         
         activateHandler = (string searchContext, VisualElement root) => {
-
             
             //Main Tree
             VisualTreeAsset main = UIElementsEditorUtility.LoadVisualTreeAsset(SISEditorConstants.MAIN_USER_SETTINGS_PATH);
@@ -32,37 +31,13 @@ internal class SISUserSettingsProvider : SettingsProvider {
             main.CloneTree(root);			
             
             //Style
-            UIElementsEditorUtility.LoadAndAddStyle( root.styleSheets, SISEditorConstants.USER_SETTINGS_STYLE_PATH);			
-            
+            UIElementsEditorUtility.LoadAndAddStyle( root.styleSheets, SISEditorConstants.USER_SETTINGS_STYLE_PATH);			           
             VisualElement content = root.Query<VisualElement>("Content");
             Assert.IsNotNull(content);				
             SISUserSettings userSettings = SISUserSettings.GetInstance();
-            int maxImagesMemoryMB = userSettings.GetMaxImagesMemoryMB();
 
             //Prepare objects for binding
-            m_maxMemoryForImagesScriptableObject       = ScriptableObject.CreateInstance<IntScriptableObject>();
-            m_maxMemoryForImagesScriptableObject.Value = userSettings.GetMaxImagesMemoryMB();
-            SerializedObject maxMemoryForImagesSerializedObject = new SerializedObject(m_maxMemoryForImagesScriptableObject);
-            
-            //Max Memory Slider
-            VisualElement fieldContainer = UIElementsUtility.AddElement<VisualElement>(content, /*className=*/"slider-field-container");            
-            m_maxMemoryForImagesSliderInt = UIElementsUtility.AddField<SliderInt, int>(fieldContainer, Contents.MAX_MEMORY_FOR_IMAGES_MB,
-                maxImagesMemoryMB);
-            
-            m_maxMemoryForImagesSliderInt.lowValue = 4096;
-            m_maxMemoryForImagesSliderInt.highValue = 131072;
-            m_maxMemoryForImagesSliderInt.bindingPath = nameof(IntScriptableObject.Value);
-            m_maxMemoryForImagesSliderInt.Bind(maxMemoryForImagesSerializedObject);			
-
-            m_maxMemoryForImagesIntField = UIElementsUtility.AddField<IntegerField, int>(fieldContainer, null,
-                maxImagesMemoryMB);
-            m_maxMemoryForImagesIntField.bindingPath = nameof(IntScriptableObject.Value);			
-            m_maxMemoryForImagesIntField.Bind(maxMemoryForImagesSerializedObject);
-            
-            Label sliderIntValuePostLabel = UIElementsUtility.AddElement<Label>(fieldContainer);			
-            sliderIntValuePostLabel.text = "MB     ";
-
-            
+            InitMaxMemoryForImagesField(content);
 
             //Buttons setup
             Button saveButton = root.Query<Button>("SaveButton");
@@ -94,7 +69,38 @@ internal class SISUserSettingsProvider : SettingsProvider {
         
     }
 
+//----------------------------------------------------------------------------------------------------------------------
 
+    void InitMaxMemoryForImagesField(VisualElement parent) {
+        
+        //Prepare objects for binding
+        SISUserSettings userSettings = SISUserSettings.GetInstance();
+        m_maxMemoryForImagesScriptableObject       = ScriptableObject.CreateInstance<IntScriptableObject>();
+        m_maxMemoryForImagesScriptableObject.Value = userSettings.GetMaxImagesMemoryMB();
+        SerializedObject maxMemoryForImagesSerializedObject = new SerializedObject(m_maxMemoryForImagesScriptableObject);
+        int maxImagesMemoryMB = userSettings.GetMaxImagesMemoryMB();
+                
+        VisualElement fieldContainer = UIElementsUtility.AddElement<VisualElement>(parent, /*className=*/"slider-field-container");
+
+        //Slider
+        m_maxMemoryForImagesSliderInt = UIElementsUtility.AddField<SliderInt, int>(fieldContainer, 
+            Contents.MAX_MEMORY_FOR_IMAGES_MB, maxImagesMemoryMB);
+            
+        m_maxMemoryForImagesSliderInt.lowValue    = 4096;
+        m_maxMemoryForImagesSliderInt.highValue   = 131072;
+        m_maxMemoryForImagesSliderInt.bindingPath = nameof(IntScriptableObject.Value);
+        m_maxMemoryForImagesSliderInt.Bind(maxMemoryForImagesSerializedObject);            
+
+        //IntField
+        m_maxMemoryForImagesIntField = UIElementsUtility.AddField<IntegerField, int>(fieldContainer, null,
+            maxImagesMemoryMB);
+        m_maxMemoryForImagesIntField.bindingPath = nameof(IntScriptableObject.Value);            
+        m_maxMemoryForImagesIntField.Bind(maxMemoryForImagesSerializedObject);
+            
+        Label sliderIntValuePostLabel = UIElementsUtility.AddElement<Label>(fieldContainer);            
+        sliderIntValuePostLabel.text = "MB     ";
+
+    }    
     
 
 //----------------------------------------------------------------------------------------------------------------------
