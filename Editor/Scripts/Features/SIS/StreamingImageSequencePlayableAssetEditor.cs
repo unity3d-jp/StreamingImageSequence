d269296f09f7cf6f5d2802fc2fc866cbd83b0910
sindharta <sindharta@gmail.com>
2020-10-22 21:23:51 +0900

    Apply Default SISPlayableAssetFPS to new SISPlayableAsset that has images

--- Editor/Scripts/Features/SIS/StreamingImageSequencePlayableAssetEditor.cs
+++ Editor/Scripts/Features/SIS/StreamingImageSequencePlayableAssetEditor.cs
@@ -65,7 +65,9 @@ internal class StreamingImageSequencePlayableAssetEditor : ClipEditor {
         if (null == clip.curves) {
             int numImages = asset.GetNumImages();
             if (numImages > 0) {
-                clip.duration = numImages * 0.125; // 8fps (standard limited animation)
+                SISUserSettings userSettings = SISUserSettings.GetInstance();
+                
+                clip.duration = (double) (numImages) / (userSettings.GetDefaultSISPlayableAssetFPS()); 
                 clip.displayName = Path.GetFileName(asset.GetFolder());
             }
             clip.CreateCurves("Curves: " + clip.displayName);
