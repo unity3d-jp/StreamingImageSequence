10bf22eaac59314b3d0465881b239c288917e9cd
sindharta <sindharta@gmail.com>
2020-10-23 15:54:06 +0900

    remove unused includes

--- Plugins~/Tests/StreamingImageSequence/SISTestMain.cpp
+++ Plugins~/Tests/StreamingImageSequence/SISTestMain.cpp
@@ -1,19 +1,5 @@
 
 #include <gtest/gtest.h>
-#include <iostream>
-//CommonLib
-#include "CommonLib/CriticalSectionType.h" //CRITICAL_SECTION_TYPE_FULL_IMAGE
-
-//Loader
-#include "StreamingImageSequence/Loader.h"
-#include "StreamingImageSequence/LoaderUtility.h"
-#include "StreamingImageSequence/ImageCatalog.h"
-
-//LoaderTest
-#include "Utilities/TestUtility.h"
-
-
-//#define ENABLE_BENCHMARK
 
 namespace StreamingImageSequencePluginTest {
 
