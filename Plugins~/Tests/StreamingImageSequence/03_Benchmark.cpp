d78551692c6aeaedbd34f8c81d70f539c3a48a9e
Sindharta Tanuwijaya <sindharta@users.noreply.github.com>
2020-10-23 16:12:05 +0900

    chore: split plugin test to several files (#252)
    * Move Utility codes to a separate folder
    * Split SISTestMain.cpp to 3 files
        ** 01_BasicImageLoadTest
        ** 02_AutoImageUnloadTest
        ** 03_Benchmark
    
    * Remove unused includes
    * Disable benchmark by default

---
+++ Plugins~/Tests/StreamingImageSequence/03_Benchmark.cpp
@@ -0,0 +1,35 @@
+
+#include <gtest/gtest.h>
+
+//LoaderTest
+#include "Utilities/TestUtility.h"
+
+//#define ENABLE_BENCHMARK
+
+namespace StreamingImageSequencePluginTest {
+
+//----------------------------------------------------------------------------------------------------------------------
+
+void BenchmarkFunc(const uint32_t loopCount, bool (*func)(), const char* msg) {
+    const clock_t t0 = clock(); 
+    for (uint32_t i=0; i< loopCount;++i) {
+        func();
+    }
+    const clock_t t1 = clock();
+    const double elapsedSec = (t1 - t0) / static_cast<double>(CLOCKS_PER_SEC);
+    std::cerr << "[          ] " << msg << " LoopCount: " << loopCount << " Elapsed: " << elapsedSec << " sec" << std::endl;
+
+};
+
+#ifdef ENABLE_BENCHMARK
+TEST(_03_Benchmark, BenchmarkLoadSpeed) {
+    BenchmarkFunc(1000, TestUtility::LoadAndUnloadTestFullPNGImage, "Loading Full PNG.");
+    BenchmarkFunc(1000, TestUtility::LoadAndUnloadTestFullTGAImage, "Loading Full TGA.");
+}
+#endif
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+} //end namespace
+ 
