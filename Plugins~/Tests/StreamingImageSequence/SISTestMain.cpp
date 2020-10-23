37670596acb5fd6bd00beff8c7b84c55b1f27776
sindharta <sindharta@gmail.com>
2020-10-23 15:53:47 +0900

    Split benchmark test to a separate file

--- Plugins~/Tests/StreamingImageSequence/SISTestMain.cpp
+++ Plugins~/Tests/StreamingImageSequence/SISTestMain.cpp
@@ -17,28 +17,6 @@
 
 namespace StreamingImageSequencePluginTest {
 
-//----------------------------------------------------------------------------------------------------------------------
-
-void BenchmarkFunc(const uint32_t loopCount, bool (*func)(), const char* msg) {
-    const clock_t t0 = clock(); 
-    for (uint32_t i=0; i< loopCount;++i) {
-        func();
-    }
-    const clock_t t1 = clock();
-    const double elapsedSec = (t1 - t0) / (double)CLOCKS_PER_SEC;
-    std::cerr << "[          ] " << msg << " LoopCount: " << loopCount << " Elapsed: " << elapsedSec << " sec" << std::endl;
-
-};
-
-#ifdef ENABLE_BENCHMARK
-TEST(Loader, BenchmarkLoadSpeed) {
-    BenchmarkFunc(1000, TestUtility::LoadAndUnloadTestFullPNGImage, "Loading Full PNG.");
-    BenchmarkFunc(1000, TestUtility::LoadAndUnloadTestFullTGAImage, "Loading Full TGA.");
-}
-#endif
-
-//----------------------------------------------------------------------------------------------------------------------
-
 int main(int argc, char** argv) {
 
     ::testing::InitGoogleTest(&argc, argv);
