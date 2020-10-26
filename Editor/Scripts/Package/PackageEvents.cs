99f4b7bee57105b5d5eb0ab1ccb35631b275d2c9
sindharta <sindharta@gmail.com>
2020-10-26 15:17:42 +0900

    Add PackageEvents handler for SIS

---
+++ Editor/Scripts/Package/PackageEvents.cs
@@ -0,0 +1,37 @@
+﻿#if UNITY_2020_2
+
+using UnityEditor;
+using UnityEditor.PackageManager;
+using UnityEngine;
+using PackageInfo = UnityEditor.PackageManager.PackageInfo;
+
+
+namespace Unity.StreamingImageSequence.Editor {
+
+internal static class PackageEvents {
+
+    [InitializeOnLoadMethod]
+    private static void PackageEvents_OnEditorLoad() {
+        Events.registeredPackages += OnPackagesRegistered;
+    }
+    
+//----------------------------------------------------------------------------------------------------------------------    
+    
+    static void OnPackagesRegistered(PackageRegistrationEventArgs packageRegistrationEventArgs) {
+        PackageInfo curPackage = packageRegistrationEventArgs.removed.FindPackage(SISEditorConstants.PACKAGE_NAME);
+
+        if (null == curPackage) {
+            curPackage = packageRegistrationEventArgs.changedTo.FindPackage(SISEditorConstants.PACKAGE_NAME);
+        }
+
+
+        if (null == curPackage) {
+            return;
+        }        
+        EditorRestartMessageNotifier.RequestOnLoadNotification(curPackage);                    
+    }
+}
+
+} //end namespace
+
+#endif //UNITY_2020_2
