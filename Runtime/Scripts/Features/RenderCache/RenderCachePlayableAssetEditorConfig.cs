036b19cedc8a79c8afaa56417a3c4c15be17f65e
sindharta <sindharta@gmail.com>
2020-11-04 19:40:24 +0900

    Add new members to set the start/end frame for capturing render cache

--- Runtime/Scripts/Features/RenderCache/RenderCachePlayableAssetEditorConfig.cs
+++ Runtime/Scripts/Features/RenderCache/RenderCachePlayableAssetEditorConfig.cs
@@ -9,10 +9,21 @@ internal class RenderCachePlayableAssetEditorConfig {
 
     internal void  SetUpdateBGColor(Color color) { m_updateBGColor = color; }
     internal Color GetUpdateBGColor()            { return m_updateBGColor; }
+
+    internal void SetCaptureStartFrame(int frame) { m_captureStartFrame = frame;}
+    internal int  GetCaptureStartFrame()          { return m_captureStartFrame;}
+    internal void SetCaptureEndFrame(int frame)   { m_captureEndFrame = frame;}
+    internal int  GetCaptureEndFrame()            { return m_captureEndFrame;}
+
+    internal void SetCaptureAllFrames(bool capture){ m_captureAllFrames = capture;}
+    internal bool GetCaptureAllFrames()            { return m_captureAllFrames;}
          
 //----------------------------------------------------------------------------------------------------------------------
     
     [HideInInspector][SerializeField] private Color m_updateBGColor = Color.black;
+    [HideInInspector][SerializeField] private int m_captureStartFrame = 0;
+    [HideInInspector][SerializeField] private int m_captureEndFrame = 0;
+    [HideInInspector][SerializeField] private bool m_captureAllFrames;
 }
 
 
