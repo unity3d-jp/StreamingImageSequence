c8bf824060bd411f34ee29e93ed583f1a9b7aa0f
sindharta <sindharta@gmail.com>
2020-11-04 18:43:41 +0900

    Add first version of RenderCachePlayableAssetEditorConfig

---
+++ Runtime/Scripts/Features/RenderCache/RenderCachePlayableAssetEditorConfig.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Unity.StreamingImageSequence {
+
+[System.Serializable]
+internal class RenderCachePlayableAssetEditorConfig {
+    
+//----------------------------------------------------------------------------------------------------------------------
+
+    internal void  SetUpdateBGColor(Color color) { m_updateBGColor = color; }
+    internal Color GetUpdateBGColor()            { return m_updateBGColor; }
+         
+//----------------------------------------------------------------------------------------------------------------------
+    
+    [HideInInspector][SerializeField] private Color m_updateBGColor = Color.black;
+}
+
+
+} //end namespace
+
