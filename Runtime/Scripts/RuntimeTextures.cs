2bec2b9fa98d34173780667d4dd2a5fb419c43f0
sindharta <sindharta@gmail.com>
2020-11-26 19:38:13 +0900

    Add RuntimeTextures class

---
+++ Runtime/Scripts/RuntimeTextures.cs
@@ -0,0 +1,34 @@
+﻿using Unity.AnimeToolbox;
+using UnityEngine;
+
+namespace Unity.StreamingImageSequence {
+
+internal static class RuntimeTextures {
+
+   
+//----------------------------------------------------------------------------------------------------------------------
+
+    internal static Texture GetTransparentTexture() {
+        if (m_transparentTexture.IsNullRef()) {
+            LoadTextures();
+        }
+        return m_transparentTexture;
+    }
+    
+    
+//----------------------------------------------------------------------------------------------------------------------
+
+    static void LoadTextures() {
+        
+        m_transparentTexture = new Texture2D (1, 1, TextureFormat.ARGB32, false); 
+        m_transparentTexture.SetPixel(0,0, Color.clear);
+        m_transparentTexture.Apply();            
+    }
+    
+//----------------------------------------------------------------------------------------------------------------------
+
+    private static Texture2D m_transparentTexture = null;
+
+}
+
+} //end namespace
