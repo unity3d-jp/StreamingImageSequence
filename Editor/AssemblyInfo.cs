bd9195bafeed1a1fe38359a54a7db8a17f6eaa58
sindharta <sindharta@gmail.com>
2021-02-04 22:30:28 +0900

    Open Assembly to VisualCompositor [skip ci]

--- Editor/AssemblyInfo.cs
+++ Editor/AssemblyInfo.cs
@@ -2,3 +2,6 @@
 
 // ADD_NEW_PLATFORM_HERE
 [assembly: InternalsVisibleTo("Unity.StreamingImageSequence.EditorTests")]
+
+[assembly: InternalsVisibleTo("Unity.VisualCompositor.Editor")]
+[assembly: InternalsVisibleTo("Unity.VisualCompositor.EditorTests")]
