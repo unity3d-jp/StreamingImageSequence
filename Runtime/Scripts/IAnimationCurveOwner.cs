afe55960160078ef60460f841a7ba2e85cc868c1
sindharta <sindharta@gmail.com>
2021-02-24 16:57:52 +0900

    Add IAnimationCurveOwner

---
+++ Runtime/Scripts/IAnimationCurveOwner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Unity.FilmInternalUtilities {
+
+
+public interface IAnimationCurveOwner {
+    void SetAnimationCurve(AnimationCurve curve);
+    AnimationCurve  GetAnimationCurve();
+}
+
+} //end namespace
