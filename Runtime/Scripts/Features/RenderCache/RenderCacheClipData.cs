a5899e585f19caeba998a3e30725ff62ba9a7013
sindharta <sindharta@gmail.com>
2021-02-24 15:04:12 +0900

    Create RenderCacheClipData

---
+++ Runtime/Scripts/Features/RenderCache/RenderCacheClipData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine.Timeline;
+
+
+namespace Unity.StreamingImageSequence {
+
+[Serializable]
+internal class RenderCacheClipData : PlayableFrameClipData {
+
+    public RenderCacheClipData() : base() { }
+
+    internal RenderCacheClipData(TimelineClip clipOwner) : base (clipOwner) {}
+
+    internal RenderCacheClipData(TimelineClip owner, SISClipData other) : base(owner, other) {}
+    
+}
+
+} //end namespace
+
+
