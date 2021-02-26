ad7d668fbabc45897eea046244b16c1536e31ac3
Sindharta Tanuwijaya <sindharta@users.noreply.github.com>
2021-02-26 14:25:22 +0900

    chore: move editor scene event handling to the Editor side (#317)
    * move editor scene event handling to the Editor side
    
    Others:
    * Rename EditorEventManager folder to EditorManager

---
+++ Editor/Scripts/EditorManagers/EditorSceneEventManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace Unity.StreamingImageSequence.Editor {
+
+[InitializeOnLoad]
+internal class EditorSceneEventManager {
+
+    static EditorSceneEventManager() {
+        EditorSceneManager.sceneClosed     += EditorSceneEventManager_OnSceneClosed;
+        EditorSceneManager.newSceneCreated += EditorSceneEventManager_OnSceneCreated;
+        EditorSceneManager.sceneOpened     += EditorSceneEventManager_OnSceneOpened;
+    }
+
+
+    private static void EditorSceneEventManager_OnSceneClosed(Scene scene) {        
+        EditorUpdateManager.ResetImageLoading(); 
+    }
+
+    private static void EditorSceneEventManager_OnSceneCreated( Scene scene, NewSceneSetup setup, NewSceneMode mode) {
+        EditorUpdateManager.ResetImageLoading();         
+    }
+
+    private static void EditorSceneEventManager_OnSceneOpened( Scene scene, OpenSceneMode mode) {
+        if (OpenSceneMode.Single != mode)
+            return;
+        EditorUpdateManager.ResetImageLoading();         
+    }
+   
+    
+//----------------------------------------------------------------------------------------------------------------------
+   
+       
+}
+
+
+} //end namespace
+
+
