515da0686175c230e0900b2642dfb0aaff94d450
Sindharta Tanuwijaya <sindharta@users.noreply.github.com>
2021-07-01 20:51:57 +0900

    chore: add a script to sync package info to the embedded packages in test projects (#351)

---
+++ Scripts~/sync_package_info.sh
@@ -0,0 +1,13 @@
+allProjects=(
+    "StreamingImageSequence_HDRP~"
+    "StreamingImageSequence_URP~"
+    "StreamingImageSequence~"
+)
+
+packageName="com.unity.streaming-image-sequence"
+
+for project in ${allProjects[@]}; do
+    cp package.json "${project}\\Packages\\${packageName}"
+    cp CHANGELOG.md "${project}\\Packages\\${packageName}"    
+done
+
