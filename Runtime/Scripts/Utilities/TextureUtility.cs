42816104a1be75cae76bc42aefac73a3932b5ade
sindharta <sindharta@gmail.com>
2021-08-17 18:05:31 +0900

    Add TextureUtility

---
+++ Runtime/Scripts/Utilities/TextureUtility.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Unity.StreamingImageSequence {
+
+internal static class TextureUtility {
+    
+    internal static Texture2D CreateTexture2D(Texture2D other, HideFlags hFlags) {
+        Texture2D tex = new Texture2D(other.width, other.height, other.format, false, false) {
+            filterMode = FilterMode.Bilinear,
+            hideFlags = hFlags,
+        };
+        return tex;
+
+    }
+}
+
+} //end namespace
