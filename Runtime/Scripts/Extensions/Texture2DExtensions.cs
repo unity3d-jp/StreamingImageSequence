0509ffc59e33d456d72fa2d41e5e3a974593a851
sindharta <sindharta@gmail.com>
2021-08-17 17:58:05 +0900

    Add Texture2DExtensions

---
+++ Runtime/Scripts/Extensions/Texture2DExtensions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Unity.StreamingImageSequence {
+
+internal static class Texture2DExtensions {
+
+    internal static bool IsEqual(this Texture2D tex, Texture2D other) {
+        return (tex.width == other.width && tex.height == other.height && tex.format == other.format);
+    }
+    
+}
+
+} //end namespace
