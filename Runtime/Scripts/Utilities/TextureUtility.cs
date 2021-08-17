0762707e2cc61b5fc5c521fdcb0e9ea6f3f28e79
sindharta <sindharta@gmail.com>
2021-08-17 18:06:44 +0900

    minor

--- Runtime/Scripts/Utilities/TextureUtility.cs
+++ Runtime/Scripts/Utilities/TextureUtility.cs
@@ -4,7 +4,7 @@ namespace Unity.StreamingImageSequence {
 
 internal static class TextureUtility {
     
-    internal static Texture2D CreateTexture2D(Texture2D other, HideFlags hFlags) {
+    internal static Texture2D CreateTexture2D(Texture2D other, HideFlags hFlags = HideFlags.None) {
         Texture2D tex = new Texture2D(other.width, other.height, other.format, false, false) {
             filterMode = FilterMode.Bilinear,
             hideFlags = hFlags,
