14549cd1f75c9a4a334e050ad567de869612b180
Sindharta Tanuwijaya <sindharta@gmail.com>
2021-09-10 12:10:50 +0900

    Reflection of EditorClip

--- Editor/Scripts/UnityEditorReflection.cs
+++ Editor/Scripts/UnityEditorReflection.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using UnityEditor;
+using System;
 
 namespace Unity.StreamingImageSequence.Editor {
 
@@ -8,6 +9,9 @@ internal static class UnityEditorReflection {
     internal static readonly MethodInfo SCROLLABLE_TEXT_AREA_METHOD 
         = typeof(EditorGUI).GetMethod("ScrollableTextAreaInternal", BindingFlags.Static | BindingFlags.NonPublic);
 
+    
+    internal static readonly Type TIMELINE_EDITOR_CLIP_TYPE = Type.GetType("UnityEditor.Timeline.EditorClip, Unity.Timeline.Editor");
+    
 }
 
 } //end namespace
