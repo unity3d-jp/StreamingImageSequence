7b66eb52b23dc5d69bbc49ad079f299526fd7fa6
Sindharta Tanuwijaya <sindharta@gmail.com>
2021-09-10 11:12:52 +0900

    Add SISTestEditorConstants

---
+++ Tests/Editor/Scripts/SISTestEditorConstants.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Unity.StreamingImageSequence.EditorTests {
+
+internal class SISTestConstants {
+    
+    internal const string TEST_TIMELINE_ASSET_PATH = "Assets/TempSISTimelineForTestRunner.playable";
+
+}
+
+} //end namespace
