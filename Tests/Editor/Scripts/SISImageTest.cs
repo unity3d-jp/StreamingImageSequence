ab495c7c449d5987c9e993f522310a0e4bc32277
Sindharta Tanuwijaya <sindharta@gmail.com>
2021-09-10 10:41:08 +0900

    Check Automatic script creation

--- Tests/Editor/Scripts/SISImageTest.cs
+++ Tests/Editor/Scripts/SISImageTest.cs
@@ -5,33 +5,38 @@ using NUnit.Framework;
 using Unity.FilmInternalUtilities;
 using Unity.StreamingImageSequence.Editor;
 using UnityEditor;
+using UnityEditor.Timeline;
 using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.TestTools;
 using UnityEngine.Timeline;
 using Assert = NUnit.Framework.Assert;
+using UIImage = UnityEngine.UI.Image;
 
 namespace Unity.StreamingImageSequence.EditorTests {
 
 internal class SISImageTest {
 
     [UnityTest]
-    public IEnumerator DuplicateImage() {
+    public IEnumerator CheckAutomaticSpriteCreation() {
         PlayableDirector                    director = EditorUtilityTest.NewSceneWithDirector();
         TimelineClip                        clip     = EditorUtilityTest.CreateTestSISTimelineClip(director);
         StreamingImageSequencePlayableAsset sisAsset = clip.asset as StreamingImageSequencePlayableAsset;
         Assert.IsNotNull(sisAsset);
 
         TrackAsset track = clip.GetParentTrack();
+        director.time = clip.start;
         Assert.IsNotNull(track);
         
-        //Create Image
+        //Create Image        
+        UIImage image0 = CreateImageWithSISRenderer(out StreamingImageSequenceRenderer sisRenderer);            
+        director.SetGenericBinding(track, sisRenderer);
+        TimelineEditor.Refresh(RefreshReason.ContentsModified);
+        yield return null;
         
-
-        director.SetGenericBinding();
-        
-        clip.GetParentTrack().set
-        
+        //Test that the sprite is automatically created
+        Assert.IsNotNull(image0.sprite, "Sprite is null");        
+        yield return null;
     
         //Cleanup
         EditorUtilityTest.DestroyTestTimelineAssets(clip);
@@ -39,8 +44,17 @@ internal class SISImageTest {
     }
 
 //----------------------------------------------------------------------------------------------------------------------
+
+
+    UIImage CreateImageWithSISRenderer(out StreamingImageSequenceRenderer sisRenderer) {
+        UIImage image = FilmInternalUtilities.ObjectUtility.CreateGameObjectWithComponent<UIImage>("");
+        Assert.IsNotNull(image);
+        
+        sisRenderer = image.gameObject.AddComponent<StreamingImageSequenceRenderer>();       
+        Assert.IsNotNull(sisRenderer);
+        return image;        
+    }
     
-    const float EPSILON = 0.001f;
     
     
 }
