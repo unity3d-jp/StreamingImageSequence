fa4b07313a2c4963b64a509cc0bad1d6d0c68915
Sindharta Tanuwijaya <sindharta@users.noreply.github.com>
2022-04-14 19:56:06 +0900

    chore: fix the script to sync package info (#428)

--- Scripts~/sync_package_info.sh
+++ Scripts~/sync_package_info.sh
@@ -7,7 +7,7 @@ allProjects=(
 packageName="com.unity.streaming-image-sequence"
 
 for project in ${allProjects[@]}; do
-    cp package.json "${project}\\Packages\\${packageName}"
-    cp CHANGELOG.md "${project}\\Packages\\${packageName}"    
+    cp package.json "${project}/Packages/${packageName}"
+    cp CHANGELOG.md "${project}/Packages/${packageName}"    
 done
 
