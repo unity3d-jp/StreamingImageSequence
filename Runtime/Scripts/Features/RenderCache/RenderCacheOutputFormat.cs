714aa8cf0a26b0e8bcb153b7fe88f677edd6bf4d
Sindharta Tanuwijaya <sindharta@users.noreply.github.com>
2022-04-18 13:51:07 +0900

    fix: package validation warnings (#434)
    * add API documentation for RenderCacheOutputFormat
    * depend on com.unity.ext.nunit@1.0.6
    * sync package info to test projects
    * update packages-lock.json

--- Runtime/Scripts/Features/RenderCache/RenderCacheOutputFormat.cs
+++ Runtime/Scripts/Features/RenderCache/RenderCacheOutputFormat.cs
@@ -4,7 +4,14 @@
 /// The supported formats for RenderCache output
 /// </summary>
 public enum RenderCacheOutputFormat {
+    /// <summary>
+    /// PNG format.
+    /// </summary>
     PNG = 0,
+    
+    /// <summary>
+    /// EXR format.
+    /// </summary>
     EXR = 1,
 }
 
