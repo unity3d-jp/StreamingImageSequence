bf1736bf9943fc931cc1c21643bcd7c37c8e313d
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-09-26 22:29:24 +0900

    increase timeout

--- Tests/Editor/Scripts/RenderCachePlayableAssetTest.cs
+++ Tests/Editor/Scripts/RenderCachePlayableAssetTest.cs
@@ -64,7 +64,7 @@ internal class RenderCachePlayableAssetTest {
         Assert.IsTrue(Directory.Exists(OUTPUT_FOLDER));
         
         //A hack to wait until the coroutine is finished
-        const int TIMEOUT_FRAMES = 60 * 5;
+        const int TIMEOUT_FRAMES = 60 * 10;
         yield return YieldEditorUtility.WaitForFramesAndIncrementUndo(TIMEOUT_FRAMES);
         
         
