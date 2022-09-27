a4bc5d0bd2a8968f9fe03c5159ae8074b4224f90
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-09-27 19:40:24 +0900

    use one ifdef

--- Runtime/Scripts/Loader/EditorCachedTextureLoader.cs
+++ Runtime/Scripts/Loader/EditorCachedTextureLoader.cs
@@ -1,11 +1,11 @@
-﻿using System.IO;
+﻿#if UNITY_EDITOR
+
+using System.IO;
 using System.Collections.Generic;
 using Unity.FilmInternalUtilities;
 using UnityEngine;
-#if UNITY_EDITOR
 using UnityEditor.SceneManagement;
 using UnityEditor;
-#endif
 
 namespace Unity.StreamingImageSequence {
 
@@ -13,23 +13,18 @@ namespace Unity.StreamingImageSequence {
 internal class EditorCachedTextureLoader {
 
     internal EditorCachedTextureLoader() {
-#if UNITY_EDITOR
         EditorSceneManager.sceneClosed += OnSceneClosed;
         m_regularAssetLoadLogger = new OneTimeLogger(() => !m_regularAssetLoaded,
             $"Can't load textures. Make sure their import settings are set to Texture2D. Folder: ");
-#endif
     }
 
-#if UNITY_EDITOR
     void OnSceneClosed(UnityEngine.SceneManagement.Scene scene) {
         UnloadAll();
     }
-#endif
     
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------    
 
     internal bool GetOrLoad(string fullPath, out ImageData imageData, out Texture2D tex) {
-#if UNITY_EDITOR
         if (fullPath.IsRegularAssetPath()) {
             
             bool isCached   = m_cachedTexturesInEditor.TryGetValue(fullPath, out tex);
@@ -52,7 +47,6 @@ internal class EditorCachedTextureLoader {
             imageData = new ImageData(StreamingImageSequenceConstants.READ_STATUS_USE_EDITOR_API);
             return true;
         }
-#endif
 
         tex       = null;
         imageData = new ImageData(StreamingImageSequenceConstants.READ_STATUS_FAIL);
@@ -62,24 +56,22 @@ internal class EditorCachedTextureLoader {
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------
     
     internal void UnloadAll() {
-#if UNITY_EDITOR
         foreach (KeyValuePair<string, Texture2D> kv in m_cachedTexturesInEditor) {
             Resources.UnloadAsset(kv.Value);
         }
         m_cachedTexturesInEditor.Clear();
-#endif
     }
     
 
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------
     
-#if UNITY_EDITOR
     private OneTimeLogger m_regularAssetLoadLogger;
     private bool          m_regularAssetLoaded = false;
     
     Dictionary<string,Texture2D> m_cachedTexturesInEditor = new Dictionary<string, Texture2D>(); //path -> Texture2D
-#endif
     
 }
 
 } //end namespace
+
+#endif
