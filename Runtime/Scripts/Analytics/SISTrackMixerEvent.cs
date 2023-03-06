5fa5fb28d7a190ef3f7387493459c1ded54621ac
Sindharta Tanuwijaya <sindharta@gmail.com>
2023-03-06 14:43:34 +0900

    add SISTrackMixerEvent

---
+++ Runtime/Scripts/Analytics/SISTrackMixerEvent.cs
@@ -0,0 +1,21 @@
+using Unity.FilmInternalUtilities;
+
+namespace Unity.StreamingImageSequence {
+
+internal class c : AnalyticsEvent<SISTrackMixerEvent.EventData> {
+
+    internal SISTrackMixerEvent(int clips, bool query) : base(new EventData { numClips = clips, }) { }
+    
+    internal struct EventData {
+        public int numClips;
+    }
+    
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------
+    
+    internal override string eventName => "streamingimagesequence_sistrack_mixer";
+    internal override int    maxItems  => 1;
+
+    
+}
+
+} //end namespace
